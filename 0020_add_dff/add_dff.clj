a99e182fa77db6792a30aced222dc77f7e2b0e3b
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2014-10-08 16:22:31 +0900

    - It listens key event.

--- 0020_add_dff/add_dff.clj
+++ 0020_add_dff/add_dff.clj
@@ -5,6 +5,7 @@
 
 (import '[java.awt Color Dimension])
 (import '[javax.swing JFrame JPanel])
+(import '[java.awt.event KeyListener KeyEvent])
 
 (def dffs '([10 10] [100 100]))
 
@@ -15,17 +16,30 @@
   (let [frame (JFrame. "add_dff")
         panel (proxy [JPanel] []
                 (paintComponent [g]
+                  (proxy-super paintComponent g)
                   (.setColor g Color/BLACK)
                   (doseq [dff dffs]
                     (let [x (dff 0) y (dff 1)]
-                      (clojure.pprint/pprint dff)
                       (.drawPolygon g
                                     (int-array [x x (+ x 40) (+ x 40)])
                                     (int-array [y (+ y 50) (+ y 50) y])
                                     4))))
                 (getPreferredSize []
-                  (Dimension. 400 400)))]
+                  (Dimension. 400 400)))
+        key-lis (proxy [KeyListener] []
+                  (keyPressed [e]
+                    (let [code (.getKeyCode e)]
+                      (cond (= code KeyEvent/VK_LEFT)  (println 'VK_LEFT)
+                            (= code KeyEvent/VK_RIGHT) (println 'VK_RIGHT)
+                            (= code KeyEvent/VK_UP)    (println 'VK_UP)
+                            (= code KeyEvent/VK_DOWN)  (println 'VK_DOWN)
+                            :else                      (println 'other)
+                            )))
+                  (keyReleased [e])
+                  (keyTyped [e]))]
     (do
+      (.setFocusable panel true)
+      (.addKeyListener panel key-lis)
       (.add frame panel)
       (.pack frame)
       ;(.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
