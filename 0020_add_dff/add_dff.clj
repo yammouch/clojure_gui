7b4210235d9107543274e55d658231b0e189885e
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2014-10-08 16:51:06 +0900

    - Arrow appears and can be controlled by arrow keys.

--- 0020_add_dff/add_dff.clj
+++ 0020_add_dff/add_dff.clj
@@ -9,39 +9,76 @@
 
 (def dffs '([10 10] [100 100]))
 
+(def direction (atom 'up))
+
 (defn -init []
   [[] (atom [])])
 
+(defn draw-arrow [g dir]
+  (let [end-size 10
+        len 50
+        [x0 y0] [200 200]
+        [x1 y1] (case dir
+                  up    [x0 (- y0 len)]
+                  down  [x0 (+ y0 len)]
+                  right [(+ x0 len) y0]
+                  left  [(- x0 len) y0])
+        [x2 y2 x3 y3] (case dir
+                        up    [(- x1 end-size) (+ y1 end-size)
+                               (+ x1 end-size) (+ y1 end-size)]
+                        down  [(- x1 end-size) (- y1 end-size)
+                               (+ x1 end-size) (- y1 end-size)]
+                        right [(- x1 end-size) (+ y1 end-size)
+                               (- x1 end-size) (- y1 end-size)]
+                        left  [(+ x1 end-size) (+ y1 end-size)
+                               (+ x1 end-size) (- y1 end-size)])]
+    (.setColor g Color/BLACK)
+    (.drawPolyline g
+                   (int-array [x0 x1])
+                   (int-array [y0 y1])
+                   2)
+    (.drawPolyline g
+                   (int-array [x2 x1 x3])
+                   (int-array [y2 y1 y3])
+                   3)))
+
+(defn make-panel []
+  (proxy [JPanel] []
+    (paintComponent [g]
+      (proxy-super paintComponent g)
+      (.setColor g Color/BLACK)
+      (doseq [dff dffs]
+        (let [x (dff 0) y (dff 1)]
+          (.drawPolygon g
+                        (int-array [x x (+ x 40) (+ x 40)])
+                        (int-array [y (+ y 50) (+ y 50) y])
+                        4)))
+      (draw-arrow g @direction))
+    (getPreferredSize []
+      (Dimension. 400 400))))
+
+(defn make-key-lis [panel]
+  (proxy [KeyListener] []
+    (keyPressed [e]
+      (let [code (.getKeyCode e)]
+        (cond (= code KeyEvent/VK_LEFT)  (reset! direction 'left)
+              (= code KeyEvent/VK_RIGHT) (reset! direction 'right)
+              (= code KeyEvent/VK_UP)    (reset! direction 'up)
+              (= code KeyEvent/VK_DOWN)  (reset! direction 'down))
+        (.repaint panel)))
+    (keyReleased [e])
+    (keyTyped [e])))
+
 (defn -main []
   (let [frame (JFrame. "add_dff")
-        panel (proxy [JPanel] []
-                (paintComponent [g]
-                  (proxy-super paintComponent g)
-                  (.setColor g Color/BLACK)
-                  (doseq [dff dffs]
-                    (let [x (dff 0) y (dff 1)]
-                      (.drawPolygon g
-                                    (int-array [x x (+ x 40) (+ x 40)])
-                                    (int-array [y (+ y 50) (+ y 50) y])
-                                    4))))
-                (getPreferredSize []
-                  (Dimension. 400 400)))
-        key-lis (proxy [KeyListener] []
-                  (keyPressed [e]
-                    (let [code (.getKeyCode e)]
-                      (cond (= code KeyEvent/VK_LEFT)  (println 'VK_LEFT)
-                            (= code KeyEvent/VK_RIGHT) (println 'VK_RIGHT)
-                            (= code KeyEvent/VK_UP)    (println 'VK_UP)
-                            (= code KeyEvent/VK_DOWN)  (println 'VK_DOWN)
-                            :else                      (println 'other)
-                            )))
-                  (keyReleased [e])
-                  (keyTyped [e]))]
-    (do
-      (.setFocusable panel true)
-      (.addKeyListener panel key-lis)
-      (.add frame panel)
-      (.pack frame)
-      ;(.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
-      (.setVisible frame true))
+        panel (make-panel)
+        key-lis (make-key-lis panel)]
+    (doto panel
+      (.setFocusable true)
+      (.addKeyListener key-lis))
+    (doto frame
+      (.add panel)
+      (.pack)
+      ;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setVisible true))
     'done))
