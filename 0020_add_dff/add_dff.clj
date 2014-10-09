cc9c69efdd37a62a274b1b782a521ce0ec28dbb6
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2014-10-09 18:48:12 +0900

    - DFF can be added and deleted.

--- 0020_add_dff/add_dff.clj
+++ 0020_add_dff/add_dff.clj
@@ -7,11 +7,9 @@
 (import '[javax.swing JFrame JPanel JOptionPane])
 (import '[java.awt.event KeyListener KeyEvent])
 
-(def dffs (atom '[[2 2] [10 10]]))
+(def dffs (atom #{{:x 2 :y 2} {:x 10 :y 10}}))
 
-;(def direction (atom 'up))
-
-(def cursor-pos (atom [5 5]))
+(def cursor-pos (atom {:x 5 :y 5}))
 (def pix-per-grid 10)
 (def mode (atom 'cursor))
 
@@ -20,9 +18,9 @@
 
 (defn draw-cursor [g pos]
   (let [cursor-size 10
-        x (- (* (pos 0) pix-per-grid)
+        x (- (* (pos :x) pix-per-grid)
              (* 0.5 cursor-size))
-        y (- (* (pos 1) pix-per-grid)
+        y (- (* (pos :y) pix-per-grid)
              (* 0.5 cursor-size))]
     (.setColor g Color/BLUE)
     (.fillOval g x y cursor-size cursor-size)))
@@ -35,12 +33,16 @@
       (.drawString g (.toString obj) 2 ypos))))
 
 (defn draw-dff [g dff color]
-  (let [[x y] (map #(* pix-per-grid (dff %)) [0 1])]
+  (let [[x y] (map #(* pix-per-grid (dff %)) [:x :y])]
     (.setColor g color)
     (.drawPolygon g
                   (int-array [x x (+ x 40) (+ x 40)])
                   (int-array [y (+ y 50) (+ y 50) y])
-                  4)))
+                  4)
+    (.drawPolyline g
+                   (int-array [(+ x 10) (+ x 20) (+ x 30)])
+                   (int-array [(+ y 50) (+ y 40) (+ y 50)])
+                   3)))
 
 (defn make-panel []
   (proxy [JPanel] []
@@ -48,19 +50,24 @@
       (proxy-super paintComponent g)
       (draw-status g [@cursor-pos @mode @dffs])
       (doseq [dff @dffs] (draw-dff g dff Color/BLACK))
-      (case @mode
-        cursor (draw-cursor g @cursor-pos)
-        dff    (draw-dff g @cursor-pos Color/RED)))
+      (cond (= @mode 'cursor) (draw-cursor g @cursor-pos)
+            (= @mode 'dff)    (draw-dff g @cursor-pos Color/RED)
+            (and (seq? @mode)
+                 (= (first @mode) 'selected))
+            (do
+              (draw-cursor g @cursor-pos)
+              (draw-dff g (second @mode) Color/RED)
+              )))
     (getPreferredSize []
       (Dimension. 400 400))))
 
 (defn move-cursor [dir]
   (reset! cursor-pos
           (case dir
-            left  (assoc @cursor-pos 0 (dec (@cursor-pos 0)))
-            right (assoc @cursor-pos 0 (inc (@cursor-pos 0)))
-            up    (assoc @cursor-pos 1 (dec (@cursor-pos 1)))
-            down  (assoc @cursor-pos 1 (inc (@cursor-pos 1))))))
+            left  (assoc @cursor-pos :x (dec (@cursor-pos :x)))
+            right (assoc @cursor-pos :x (inc (@cursor-pos :x)))
+            up    (assoc @cursor-pos :y (dec (@cursor-pos :y)))
+            down  (assoc @cursor-pos :y (inc (@cursor-pos :y))))))
 
 (defn change-mode []
   (cond (= @mode 'cursor) (reset! mode 'dff)
@@ -80,11 +87,28 @@
               (#{KeyEvent/VK_RIGHT KeyEvent/VK_L} code) (move-cursor 'right)
               (#{KeyEvent/VK_UP    KeyEvent/VK_K} code) (move-cursor 'up)
               (#{KeyEvent/VK_DOWN  KeyEvent/VK_J} code) (move-cursor 'down)
-              (= code KeyEvent/VK_M)                    (change-mode)
               (= code KeyEvent/VK_Q)                    (close-window frame))
-        (when (and (= @mode 'dff)
-                   (= code KeyEvent/VK_ENTER))
-          (swap! dffs conj @cursor-pos)))
+        (cond (= @mode 'cursor)
+              (cond (= code KeyEvent/VK_A)
+                    (reset! mode 'dff)
+                    (and (= code KeyEvent/VK_ENTER)
+                         (@dffs @cursor-pos))
+                    (reset! mode (list 'selected @cursor-pos))
+                    ))
+        (cond (= @mode 'dff)
+              (cond (= code KeyEvent/VK_ENTER)
+                    (swap! dffs conj @cursor-pos)
+                    (= code KeyEvent/VK_ESCAPE)
+                    (reset! mode 'cursor)
+                    ))
+        (cond (and (seq? @mode)
+                   (= (first @mode) 'selected))
+              (cond (= code KeyEvent/VK_X)
+                    (do
+                      (swap! dffs disj (second @mode))
+                      (reset! mode 'cursor))
+                    (= code KeyEvent/VK_ESCAPE)
+                    (reset! mode 'cursor))))
       (.repaint panel))
     (keyReleased [e])
     (keyTyped [e])))
