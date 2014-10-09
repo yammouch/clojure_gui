32c49c778e4f7a568fafdaa7d69662e276a12279
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2014-10-09 17:33:20 +0900

    - Dff can be added.

--- 0020_add_dff/add_dff.clj
+++ 0020_add_dff/add_dff.clj
@@ -7,7 +7,7 @@
 (import '[javax.swing JFrame JPanel JOptionPane])
 (import '[java.awt.event KeyListener KeyEvent])
 
-(def dffs '([10 10] [100 100]))
+(def dffs (atom '[[2 2] [10 10]]))
 
 ;(def direction (atom 'up))
 
@@ -18,31 +18,39 @@
 (defn -init []
   [[] (atom [])])
 
+(defn draw-cursor [g pos]
+  (let [cursor-size 10
+        x (- (* (pos 0) pix-per-grid)
+             (* 0.5 cursor-size))
+        y (- (* (pos 1) pix-per-grid)
+             (* 0.5 cursor-size))]
+    (.setColor g Color/BLUE)
+    (.fillOval g x y cursor-size cursor-size)))
+
 (let [font (Font. Font/MONOSPACED Font/PLAIN 12)]
-  (defn draw-cursor [g pos]
-    (let [cursor-size 10
-          x (- (* (pos 0) pix-per-grid)
-               (* 0.5 cursor-size))
-          y (- (* (pos 1) pix-per-grid)
-               (* 0.5 cursor-size))]
-      (.setColor g Color/BLUE)
-      (.setFont g font)
-      (.drawString g (.toString @cursor-pos) 2 12)
-      (.drawString g (.toString @mode) 2 22)
-      (.fillOval g x y cursor-size cursor-size))))
+  (defn draw-status [g objs]
+    (.setColor g Color/BLUE)
+    (.setFont g font)
+    (doseq [[obj ypos] (map #(list %1 (+ 12 (* 12 %2))) objs (range))]
+      (.drawString g (.toString obj) 2 ypos))))
+
+(defn draw-dff [g dff color]
+  (let [[x y] (map #(* pix-per-grid (dff %)) [0 1])]
+    (.setColor g color)
+    (.drawPolygon g
+                  (int-array [x x (+ x 40) (+ x 40)])
+                  (int-array [y (+ y 50) (+ y 50) y])
+                  4)))
 
 (defn make-panel []
   (proxy [JPanel] []
     (paintComponent [g]
       (proxy-super paintComponent g)
-      (.setColor g Color/BLACK)
-      (doseq [dff dffs]
-        (let [x (dff 0) y (dff 1)]
-          (.drawPolygon g
-                        (int-array [x x (+ x 40) (+ x 40)])
-                        (int-array [y (+ y 50) (+ y 50) y])
-                        4)))
-      (draw-cursor g @cursor-pos))
+      (draw-status g [@cursor-pos @mode @dffs])
+      (doseq [dff @dffs] (draw-dff g dff Color/BLACK))
+      (case @mode
+        cursor (draw-cursor g @cursor-pos)
+        dff    (draw-dff g @cursor-pos Color/RED)))
     (getPreferredSize []
       (Dimension. 400 400))))
 
@@ -73,7 +81,10 @@
               (#{KeyEvent/VK_UP    KeyEvent/VK_K} code) (move-cursor 'up)
               (#{KeyEvent/VK_DOWN  KeyEvent/VK_J} code) (move-cursor 'down)
               (= code KeyEvent/VK_M)                    (change-mode)
-              (= code KeyEvent/VK_Q)                    (close-window frame)))
+              (= code KeyEvent/VK_Q)                    (close-window frame))
+        (when (and (= @mode 'dff)
+                   (= code KeyEvent/VK_ENTER))
+          (swap! dffs conj @cursor-pos)))
       (.repaint panel))
     (keyReleased [e])
     (keyTyped [e])))
