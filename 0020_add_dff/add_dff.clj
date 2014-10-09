056328834b71f9d1989861aadb73f28701c926c7
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2014-10-09 10:27:13 +0900

    - A cursor appears and can be controlled by cursor keys.

--- 0020_add_dff/add_dff.clj
+++ 0020_add_dff/add_dff.clj
@@ -11,36 +11,48 @@
 
 (def direction (atom 'up))
 
+(def cursor-pos (atom [5 5]))
+(def pix-per-grid 10)
+
 (defn -init []
   [[] (atom [])])
 
-(defn draw-arrow [g dir]
-  (let [end-size 10
-        len 50
-        [x0 y0] [200 200]
-        [x1 y1] (case dir
-                  up    [x0 (- y0 len)]
-                  down  [x0 (+ y0 len)]
-                  right [(+ x0 len) y0]
-                  left  [(- x0 len) y0])
-        [x2 y2 x3 y3] (case dir
-                        up    [(- x1 end-size) (+ y1 end-size)
-                               (+ x1 end-size) (+ y1 end-size)]
-                        down  [(- x1 end-size) (- y1 end-size)
-                               (+ x1 end-size) (- y1 end-size)]
-                        right [(- x1 end-size) (+ y1 end-size)
-                               (- x1 end-size) (- y1 end-size)]
-                        left  [(+ x1 end-size) (+ y1 end-size)
-                               (+ x1 end-size) (- y1 end-size)])]
-    (.setColor g Color/BLACK)
-    (.drawPolyline g
-                   (int-array [x0 x1])
-                   (int-array [y0 y1])
-                   2)
-    (.drawPolyline g
-                   (int-array [x2 x1 x3])
-                   (int-array [y2 y1 y3])
-                   3)))
+(defn draw-cursor [g pos]
+  (let [cursor-size 10
+        x (- (* (pos 0) pix-per-grid)
+             (* 0.5 cursor-size))
+        y (- (* (pos 1) pix-per-grid)
+             (* 0.5 cursor-size))]
+    (.setColor g Color/BLUE)
+    (.fillOval g x y cursor-size cursor-size)))
+
+;(defn draw-arrow [g dir]
+;  (let [end-size 10
+;        len 50
+;        [x0 y0] [200 200]
+;        [x1 y1] (case dir
+;                  up    [x0 (- y0 len)]
+;                  down  [x0 (+ y0 len)]
+;                  right [(+ x0 len) y0]
+;                  left  [(- x0 len) y0])
+;        [x2 y2 x3 y3] (case dir
+;                        up    [(- x1 end-size) (+ y1 end-size)
+;                               (+ x1 end-size) (+ y1 end-size)]
+;                        down  [(- x1 end-size) (- y1 end-size)
+;                               (+ x1 end-size) (- y1 end-size)]
+;                        right [(- x1 end-size) (+ y1 end-size)
+;                               (- x1 end-size) (- y1 end-size)]
+;                        left  [(+ x1 end-size) (+ y1 end-size)
+;                               (+ x1 end-size) (- y1 end-size)])]
+;    (.setColor g Color/BLACK)
+;    (.drawPolyline g
+;                   (int-array [x0 x1])
+;                   (int-array [y0 y1])
+;                   2)
+;    (.drawPolyline g
+;                   (int-array [x2 x1 x3])
+;                   (int-array [y2 y1 y3])
+;                   3)))
 
 (defn make-panel []
   (proxy [JPanel] []
@@ -53,19 +65,28 @@
                         (int-array [x x (+ x 40) (+ x 40)])
                         (int-array [y (+ y 50) (+ y 50) y])
                         4)))
-      (draw-arrow g @direction))
+      (draw-cursor g @cursor-pos))
     (getPreferredSize []
       (Dimension. 400 400))))
 
 (defn make-key-lis [panel]
   (proxy [KeyListener] []
     (keyPressed [e]
-      (let [code (.getKeyCode e)]
-        (cond (= code KeyEvent/VK_LEFT)  (reset! direction 'left)
-              (= code KeyEvent/VK_RIGHT) (reset! direction 'right)
-              (= code KeyEvent/VK_UP)    (reset! direction 'up)
-              (= code KeyEvent/VK_DOWN)  (reset! direction 'down))
-        (.repaint panel)))
+      (let [code (.getKeyCode e)
+            new-pos (cond (= code KeyEvent/VK_LEFT)
+                          (assoc @cursor-pos 0 (dec (@cursor-pos 0)))
+                          (= code KeyEvent/VK_RIGHT)
+                          (assoc @cursor-pos 0 (inc (@cursor-pos 0)))
+                          (= code KeyEvent/VK_DOWN)
+                          (assoc @cursor-pos 1 (inc (@cursor-pos 1)))
+                          (= code KeyEvent/VK_UP)
+                          (assoc @cursor-pos 1 (dec (@cursor-pos 1))))]
+        (reset! cursor-pos new-pos))
+        ;(cond (= code KeyEvent/VK_LEFT)  (reset! direction 'left)
+        ;      (= code KeyEvent/VK_RIGHT) (reset! direction 'right)
+        ;      (= code KeyEvent/VK_UP)    (reset! direction 'up)
+        ;      (= code KeyEvent/VK_DOWN)  (reset! direction 'down))
+      (.repaint panel))
     (keyReleased [e])
     (keyTyped [e])))
 
