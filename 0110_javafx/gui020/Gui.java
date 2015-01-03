fd434b4e04deb6d2de75a0b6500bac5e3c42ba62
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2015-01-03 18:57:25 +0900

    - First checkin.

---
+++ 0110_javafx/gui020/Gui.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override public void start(Stage stage) {
+    BorderPane root;
+    try {
+      root = (BorderPane)FXMLLoader.load(getClass().getResource("gui.fxml"));
+      Scene scene = new Scene(root, 200, 100);
+      stage.setScene(scene);
+      stage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
