45ea043d0e8785da11b942d36af8eaed73fea911
unknown <TYamauch@tyamauch-l01.ad.analog.com>
2015-01-03 20:54:55 +0900

    - First checkin.

---
+++ 0110_javafx/gui010/Gui.clj
@@ -0,0 +1,31 @@
+(ns Gui)
+
+(import '[javafx.scene SceneBuilder])
+(import '[javafx.scene.control ButtonBuilder])
+(import '[javafx.scene.layout VBoxBuilder])
+(import '[javafx.stage StageBuilder])
+
+(defonce force-toolkit-init (javafx.embed.swing.JFXPanel.))
+
+(def stage (atom nil))
+
+(defn make-stage []
+  (let [button (.. ButtonBuilder create
+                   (text "Say \"Hello Clojure\"!")
+                   build)
+        vbox (.. VBoxBuilder create
+                 (minHeight 480) (minWidth 640)
+                 (children [button]) build)
+        scene (.. SceneBuilder create
+                  (height 480) (width 640)
+                  (root vbox) build)
+        stage-local (.. StageBuilder create
+                        (title "Hello JavaFX")
+                        (scene scene) build)]
+    (reset! stage stage-local)
+    ))
+
+(defn -main []
+  (javafx.application.Platform/runLater make-stage)
+  (javafx.application.Platform/runLater #(.show @stage)))
+
