1471bdf79227b3c9ff28071db05e383bc5966977
unknown <tyamauch@tyamauch-l01.ad.analog.com>
2015-01-07 15:41:53 +0900

    - Copied ../gui070.

---
+++ 0110_javafx/gui080/Gui.clj
@@ -0,0 +1,30 @@
+; reference:
+; http://libro.tuyano.com/index3?id=12496003&page=4
+
+(ns Gui)
+
+(import
+  '(java.io IOException)
+  '(javafx.application Application)
+  '(javafx.fxml FXMLLoader)
+  '(javafx.scene Scene)
+  '(javafx.stage Stage))
+
+(gen-class
+  :name "Gui"
+  :main true
+  :extends javafx.application.Application)
+
+(defn -start [this ^Stage stage]
+  (try
+    (let [root (FXMLLoader/load (. (. this getClass)
+                                   getResource "gui.fxml"))
+          scene (Scene. root 200 100)]
+      (println (.toString root))
+      (.setScene stage scene)
+      (.show stage))
+    (catch IOException e (.printStackTrace e))
+    ))
+
+(defn -main [& args]
+  (Application/launch (Class/forName "Gui") (into-array String [])))
