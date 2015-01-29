204672e5e800feeb0ee2456f892085b3b9f46d31
Takuya Yamauchi <yammouch@gmail.com>
2015-01-29 15:38:38 +0900

    - Modified.

--- 0130_menu/compile.clj
+++ 0130_menu/compile.clj
@@ -1,7 +1,8 @@
 ; do following
 ; $ mkdir classes
 ; $ java -cp .:/path/to/clojure-1.6.0.jar clojure.main -i compile.clj
-; (on DOS prompt...)
+; $ java -cp ./classes:/path/to/clojure-1.6.0.jar SchemRtl
+; (for DOS ...)
 ; > java -cp classes;\path\to\clojure-1.6.0.jar SchemRtl
 
 (def force-toolkit-init (javafx.embed.swing.JFXPanel.))
