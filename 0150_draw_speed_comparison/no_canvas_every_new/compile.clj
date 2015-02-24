00490fc5d015bcef8668e15f003115eac3a69ef7
Takuya Yamauchi <yammouch@gmail.com>
2015-02-24 22:26:01 +0900

    - Updated according to modified class and file names.

--- 0150_draw_speed_comparison/no_canvas_every_new/compile.clj
+++ 0150_draw_speed_comparison/no_canvas_every_new/compile.clj
@@ -1,10 +1,10 @@
 ; do following
 ; $ java -cp .:/path/to/clojure-1.6.0.jar clojure.main -i compile.clj
-; $ java -cp ./classes:/path/to/clojure-1.6.0.jar SchemRtl
+; $ java -cp ./classes:/path/to/clojure-1.6.0.jar no_canvas_every_new
 ; (for DOS ...)
 ; > java -cp .;\path\to\clojure-1.6.0.jar clojure.main -i compile.clj
-; > java -cp classes;\path\to\clojure-1.6.0.jar SchemRtl
+; > java -cp classes;\path\to\clojure-1.6.0.jar no_canvas_every_new
 
 (def force-toolkit-init (javafx.embed.swing.JFXPanel.))
-(compile 'SchemRtl)
+(compile 'no_canvas_every_new)
 (System/exit 0)
