e6e14f65ba5795ab1b524dad5546c7728fe140b8
unknown <tyam@N34125.office.amsiag.com>
2015-03-06 04:47:18 +0100

    - Updated.

--- 0140_refactoring/compile_tests.clj
+++ 0140_refactoring/compile_tests.clj
@@ -1,9 +1,9 @@
 ; do following
 ; $ java -cp .:/path/to/clojure-1.6.0.jar clojure.main -i compile_tests.clj
-; $ java -cp ./classes_tests:/path/to/clojure-1.6.0.jar {testcase}
+; $ java -cp ./classes_tests:/path/to/clojure-1.6.0.jar tests.{testcase}
 ; (for DOS ...)
 ; > java -cp .;\path\to\clojure-1.6.0.jar clojure.main -i compile_tests.clj
-; > java -cp classes_tests;\path\to\clojure-1.6.0.jar {testcase}
+; > java -cp classes_tests;\path\to\clojure-1.6.0.jar tests.{testcase}
 
 (def force-toolkit-init (javafx.embed.swing.JFXPanel.))
 (binding [*compile-path* "classes_tests"]
