69ad7db9f43212938857dc470ef51d4665ab9d4a
unknown <tyam@N34125.office.amsiag.com>
2015-03-06 07:16:24 +0100

    - Compile passes.

--- 0140_refactoring/compile.clj
+++ 0140_refactoring/compile.clj
@@ -6,5 +6,8 @@
 ; > java -cp classes;\path\to\clojure-1.6.0.jar SchemRtl
 
 (def force-toolkit-init (javafx.embed.swing.JFXPanel.))
+(compile 'LogicElements)
+(compile 'LogicElementsDraw)
+(compile 'SchemDialog)
 (compile 'SchemRtl)
 (System/exit 0)
