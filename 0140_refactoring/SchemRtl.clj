ae285b6822db8540edd426c5574a65a07da23789
unknown <tyam@N34125.office.amsiag.com>
2015-03-06 06:56:58 +0100

    - Logic Element manipulating function have been put away into   LogicElement.clj

--- 0140_refactoring/SchemRtl.clj
+++ 0140_refactoring/SchemRtl.clj
@@ -25,6 +25,8 @@
 (require 'clojure.pprint)
 (require 'SchemDialog)
 (alias 'sd 'SchemDialog)
+(require 'LogicElements)
+(alias 'lel 'LogicElements)
 
 (def pix-per-grid 8.0)
 (defn grid2screen [[grid-x grid-y]]
@@ -190,51 +192,7 @@
 ;--------------------------------------------------
 ; generic functions for lel (Logic ELement)
 ;--------------------------------------------------
-
-(defn lel-init [type]
-  (case type
-    :in    {:type :in    :x 0 :y 0 :direction :right}
-    :out   {:type :out   :x 0 :y 0 :direction :right}
-    :inout {:type :inout :x 0 :y 0 :direction :horizontal}
-    :dot   {:type :dot   :x 0 :y 0}
-    :name  {:type :name  :x 0 :y 0
-            :string "blah" :v-align :bottom :h-align :left}
-    :not   {:type :not   :x 0 :y 0 :direction :right}
-    :and   {:type :and   :x 0 :y 0 :direction :right :width 4 :height 4}
-    :or    {:type :or    :x 0 :y 0 :direction :right :width 4 :height 4}
-    :dff   {:type :dff   :x 0 :y 0}
-    :dffr  {:type :dffr  :x 0 :y 0}
-    :mux21 {:type :mux21 :x 0 :y 0 :direction :right :width 2 :height 6
-            :order01 :0->1}
-    :mux-n {:type :mux-n :x 0 :y 0 :direction :right :width 2 :height 6}
-    :plus  {:type :plus  :x 0 :y 0}
-    :minus {:type :minus :x 0 :y 0}
-    ))
-
-(defmulti lel-width  (fn [lel] (:type lel)))
-(defmulti lel-height (fn [lel] (:type lel)))
-(defmulti lel-x-min  (fn [lel] (:type lel)))
-(defmulti lel-x-max  (fn [lel] (:type lel)))
-(defmulti lel-y-min  (fn [lel] (:type lel)))
-(defmulti lel-y-max  (fn [lel] (:type lel)))
-(defmulti lel-draw   (fn [lel color & xs] (:type lel)))
-
-(defmethod lel-width  :default [lel]
-  (case (lel :direction)
-    (:up :down :vertical) (lel :height)
-    (lel :width)))
-(defmethod lel-height :default [lel]
-  (case (lel :direction)
-    (:up :down :vertical) (lel :width)
-    (lel :height)))
-(defmethod lel-x-min  :default [lel] (:x lel))
-(defmethod lel-x-max  :default [lel] (+ (:x lel) (lel-width lel)))
-(defmethod lel-y-min  :default [lel] (:y lel))
-(defmethod lel-y-max  :default [lel] (+ (:y lel) (lel-height lel)))
-
-; Following declarations look redundant at this commit.
-; But width and height will be variables after adding some size change
-; features.
+(defmulti lel-draw (fn [lel color & xs] (:type lel)))
 
 (defn unidirectional-port-symbol [lel color]
   (let [symbol (Polygon. (double-array (apply concat
@@ -244,53 +202,36 @@
                      [[0 0] [2 0] [3 1] [2 2] [0 2]]
                      ))))]
     (doto symbol (.setStroke color) (.setFill Color/TRANSPARENT))
-    [(draw-text {:x (+ (:x lel) (* 0.5 (lel-width  lel)))
-                 :y (+ (:y lel) (* 0.5 (lel-height lel)))}
+    [(draw-text {:x (+ (:x lel) (* 0.5 (lel/width  lel)))
+                 :y (+ (:y lel) (* 0.5 (lel/height lel)))}
                 (case (:type lel) :in "I", :out "O")
                 color :center :center)
      symbol]))
 
 ; for "in"
-(defmethod lel-width  :in [lel]
-  (case (lel :direction) (:right :left) 3, (:up :down) 2))
-(defmethod lel-height :in [lel]
-  (case (lel :direction) (:right :left) 2, (:up :down) 3))
-(defmethod lel-draw   :in [lel color] (unidirectional-port-symbol lel color))
+(defmethod lel-draw :in [lel color] (unidirectional-port-symbol lel color))
 
 ; for "out"
-(defmethod lel-width  :out [lel]
-  (case (lel :direction) (:right :left) 3, (:up :down) 2))
-(defmethod lel-height :out [lel]
-  (case (lel :direction) (:right :left) 2, (:up :down) 3))
-(defmethod lel-draw   :out [lel color] (unidirectional-port-symbol lel color))
+(defmethod lel-draw :out [lel color] (unidirectional-port-symbol lel color))
 
 ; for "inout"
-(defmethod lel-width  :inout [lel]
-  (case (lel :direction) :horizontal 3, :vertical 2))
-(defmethod lel-height :inout [lel]
-  (case (lel :direction) :horizontal 2, :vertical 3))
-(defmethod lel-draw   :inout [lel color]
+(defmethod lel-draw :inout [lel color]
   (let [symbol (Polygon. (double-array
                 (mapcat #(grid2screen (map + (rotate-ofs % 3 2 (lel :direction))
                                              [(:x lel) (:y lel)]))
                         [[0 1] [1 0] [2 0] [3 1] [2 2] [1 2]]
                         )))]
     (doto symbol (.setStroke color) (.setFill Color/TRANSPARENT))
-    [(draw-text {:x (+ (:x lel) (* 0.5 (lel-width  lel)))
-                 :y (+ (:y lel) (* 0.5 (lel-height lel)))}
+    [(draw-text {:x (+ (:x lel) (* 0.5 (lel/width  lel)))
+                 :y (+ (:y lel) (* 0.5 (lel/height lel)))}
                 "IO" color :center :center)
      symbol]))
 
 ; for "dot"
-(defmethod lel-width  :dot [lel] 0)
-(defmethod lel-height :dot [lel] 0)
-(defmethod lel-draw   :dot [lel color] [(draw-dot lel 7 color)])
+(defmethod lel-draw :dot [lel color] [(draw-dot lel 7 color)])
 
 ; for "name"
-; 0 of size is feasible?
-(defmethod lel-width  :name [lel] 0)
-(defmethod lel-height :name [lel] 0)
-(defmethod lel-draw   :name [lel color]
+(defmethod lel-draw :name [lel color]
   (let [[x y] (grid2screen [(:x lel) (:y lel)])
         line-h (Line. (- x 1.0) y (+ x 1.0) y)
         line-v (Line. x (- y 1.0) x (+ y 1.0))]
@@ -300,11 +241,7 @@
      line-h line-v]))
 
 ; for "not"
-(defmethod lel-width  :not [lel]
-  (case (lel :direction) (:right :left) 3, (:up :down) 4))
-(defmethod lel-height :not [lel]
-  (case (lel :direction) (:right :left) 4, (:up :down) 3))
-(defmethod lel-draw   :not [lel color]
+(defmethod lel-draw :not [lel color]
   (let [[[x0 y0] [x1 y1] [x2 y2] [cx cy]]
           (map #(grid2screen (map + (rotate-ofs % 3 4 (lel :direction))
                                     [(:x lel) (:y lel)]))
@@ -316,7 +253,6 @@
     [triangle circle]))
 
 ; for "and"
-; "and" should be extended according to needed inputs.
 (defmethod lel-draw :and [lel color]
   (let [w (lel :width) h (lel :height)
         [[x0 y0] [x1 y1] [x2 y2] [x3 y3]]
@@ -344,7 +280,6 @@
     [symbol]))
 
 ; for "or"
-; "or" should be extended according to needed inputs.
 (defmethod lel-draw :or [lel color]
   (let [w (lel :width) h (lel :height)
         [[x0 y0] [x1 y1]]
@@ -367,8 +302,6 @@
     [symbol]))
 
 ; for "dff"
-(defmethod lel-width  :dff [lel] 4)
-(defmethod lel-height :dff [lel] 5)
 (defmethod lel-draw   :dff [lel color]
   (let [[x y] (grid2screen [(:x lel) (:y lel)])
         rect (Rectangle. x y (* 4 pix-per-grid) (* 5 pix-per-grid))
@@ -380,8 +313,6 @@
     [rect line]))
 
 ; for "dffr"
-(defmethod lel-width  :dffr [lel] 4)
-(defmethod lel-height :dffr [lel] 5)
 (defmethod lel-draw   :dffr [lel color]
   (conj (lel-draw (assoc lel :type :dff) color)
         (draw-text {:x (+ (:x lel) 2) :y (:y lel)}
@@ -420,20 +351,16 @@
   (let [[x y] (grid2screen [(:x lel) (:y lel)])
         rect (Rectangle. x y (* 4 pix-per-grid) (* 4 pix-per-grid))]
     (doto rect (.setStroke color) (.setFill Color/TRANSPARENT))
-    [(draw-text {:x (+ (lel :x) (* 0.5 (lel-width  lel)))
-                 :y (+ (lel :y) (* 0.5 (lel-height lel)))}
+    [(draw-text {:x (+ (lel :x) (* 0.5 (lel/width  lel)))
+                 :y (+ (lel :y) (* 0.5 (lel/height lel)))}
                 (case (:type lel) :plus "+", :minus "-")
                 color :center :center)
      rect]))
 
 ; for "plus"
-(defmethod lel-width  :plus [lel] 4)
-(defmethod lel-height :plus [lel] 4)
 (defmethod lel-draw   :plus [lel color] (arith-symbol lel color))
 
 ; for "minus"
-(defmethod lel-width  :minus [lel] 4)
-(defmethod lel-height :minus [lel] 4)
 (defmethod lel-draw   :minus [lel color] (arith-symbol lel color))
 
 ;--------------------------------------------------
@@ -488,7 +415,7 @@
    (concat
     (map (fn [[k v]] (draw-wire v Color/BLACK)) @wires)
     (mapcat (fn [[k v]] (lel-draw v Color/BLACK)) @lels)
-    (lel-draw (conj (lel-init (:type @mode))
+    (lel-draw (conj (lel/lel-init (:type @mode))
                     @cursor-pos)
               Color/RED))))
 
@@ -523,12 +450,12 @@
     (into-array Node
      (concat
       (mapcat (fn [{idx0 :idx0 idx1 :idx1 part :part}]
-                (let [lel (lel-init part)]
+                (let [lel (lel/lel-init part)]
                   (lel-draw (conj lel
                                   {:x (- (+ (* 10 idx1) 6)
-                                         (int (/ (lel-width lel) 2)))
+                                         (int (/ (lel/width lel) 2)))
                                    :y (- (+ (* 10 idx0) 6)
-                                         (int (/ (lel-height lel) 2))
+                                         (int (/ (lel/height lel) 2))
                                          )})
                             Color/BLACK)))
               parts)
@@ -550,33 +477,11 @@
 (defn move-cursor [dir speed]
   (dosync (alter cursor-pos #(update-in % [dir] (partial + speed)))))
 
-(defn move-lels [lels dir speed]
-  (reduce #(update-in %1 [%2 dir] (partial + speed)) lels (keys lels)))
-
-(defn move-wire [wire dir speed points]
-  (let [[& coords] (case points
-                     #{p0}    (case dir :x [:x0    ] :y [:y0    ])
-                     #{p1}    (case dir :x [    :x1] :y [    :y1])
-                     #{p0 p1} (case dir :x [:x0 :x1] :y [:y0 :y1]))]
-    (reduce #(update-in %1 [%2] (partial + speed))
-            wire coords)))
-
-(defn move-selected-wires [dir speed]
-  (let [moved (reduce (fn [wires [sel points]]
-                        (update-in wires [sel] move-wire dir speed points))
-                      @wires @moving-vertices)]
-    (dosync
-      (alter moving-wires
-             #(reduce (fn [wires k]
-                        (update-in wires [k] move-wire dir speed '#{p0 p1}))
-                      % (keys %)))
-      (ref-set wires moved)
-      )))
-
 (defn move-selected [dir speed]
   (dosync
-    (alter moving-lels move-lels dir speed))
-  (move-selected-wires dir speed))
+    (alter moving-lels lel/move-lels dir speed)
+    (alter moving-wires lel/move-wires dir speed)
+    (alter wires lel/move-wires-by-vertices @moving-vertices dir speed)))
 
 (defn move-catalog [dir speed]
   (dosync (alter catalog-pos
@@ -591,75 +496,6 @@
   (dosync
     (ref-set selected-lels #{})
     (ref-set selected-wires {})))
-
-(defn find-lel-by-pos [lels pos]
-  (some (fn [[k v]]
-          (when (= (map #(pos %) [:x :y]) (map #(v %) [:x :y])) k))
-        lels))
-
-(defn wire-vs-cursor [wire cur]
-  (let [fcomp (fn [qc q0 q1]
-                (let [[q0 q1 inv] (if (< q0 q1)
-                                    [q0 q1 false]
-                                    [q1 q0 true ])]
-                  (cond (< qc q0) nil
-                        (< q1 qc) nil
-
-                        (< (- q1 q0) 4)
-                        (cond (= qc q0) (if inv #{'p1} #{'p0})
-                              (= qc q1) (if inv #{'p0} #{'p1})
-                              :else     #{'p0 'p1})
-
-                        (<= qc (+ q0 1)) (if inv #{'p1} #{'p0})
-                        (<= (- q1 1) qc) (if inv #{'p0} #{'p1})
-                        :else #{'p0 'p1}
-                        )))]
-    (cond (= (map #(cur %) [:x :y]) (map #(wire %) [:x0 :y0])) #{'p0}
-          (= (map #(cur %) [:x :y]) (map #(wire %) [:x1 :y1])) #{'p1}
-
-          (= (:x cur) (:x0 wire) (:x1 wire))
-          (fcomp (:y cur) (:y0 wire) (:y1 wire))
-
-          (= (:y cur) (:y0 wire) (:y1 wire))
-          (fcomp (:x cur) (:x0 wire) (:x1 wire))
-
-          :else nil)))
-
-(defn find-wires-by-pos [wires pos]
-  (reduce-kv (fn [acc k v] (if-let [p (wire-vs-cursor v pos)]
-                             (conj acc {k p}) acc))
-             {} wires))
-
-(defn merge-selected-wire [base add]
-  (reduce-kv (fn [acc k points]
-               (if (base k)
-                 (update-in acc [k] #(into % points))
-                 (conj acc {k points})))
-             base add))
-
-; An edge of a line should be selected by surrounding it.
-(defn rectangular-select [lels wires x0 y0 x1 y1]
-  (let [xmin (min x0 x1) xmax (max x0 x1)
-        ymin (min y0 y1) ymax (max y0 y1)
-        lels (filter (fn [[k v]]
-                       (and (<= xmin (lel-x-min v)) (<= (lel-x-max v) xmax)
-                            (<= ymin (lel-y-min v)) (<= (lel-y-max v) ymax)))
-                     lels)
-        wires (filter (fn [[k v]]
-                        (and (<= xmin (min (:x0 v) (:x1 v)))
-                             (<= (max (:x0 v) (:x1 v)) xmax)
-                             (<= ymin (min (:y0 v) (:y1 v)))
-                             (<= (max (:y0 v) (:y1 v)) ymax)))
-                      wires)]
-    {:lels (set (keys lels))
-     :wires (zipmap (keys wires) (repeat '#{p0 p1}))
-     }))
-
-(defn remove-lel-by-key [lels keys]
-  (into {} (remove (fn [[k _]] (keys k)) lels)))
-
-(defn remove-wire-by-key [wires keys]
-  (into {} (remove (fn [[k _]] (= (keys k) 'p0p1)) wires)))
 
 ;;(defn close-window [frame]
 ;;  (let [yn (JOptionPane/showConfirmDialog
@@ -733,10 +569,10 @@
                                  :rect-y0 (@cursor-pos :y)}))))
    KeyCode/ENTER
    (fn [_]
-     (let [lel-key (find-lel-by-pos @lels @cursor-pos)
-           wire-key (find-wires-by-pos @wires @cursor-pos)
+     (let [lel-key (lel/find-lel-by-pos @lels @cursor-pos)
+           wire-key (lel/find-wires-by-pos @wires @cursor-pos)
            rect-keys (if (@mode :rect-x0)
-                       (rectangular-select @lels @wires
+                       (lel/rectangular-select @lels @wires
                          (@mode :rect-x0) (@mode :rect-y0)
                          (@cursor-pos :x) (@cursor-pos :y))
                        {})]
@@ -746,17 +582,17 @@
          (when (:lels rect-keys)
            (alter selected-lels into (:lels rect-keys)))
          (when wire-key
-           (alter selected-wires merge-selected-wire wire-key))
+           (alter selected-wires lel/merge-selected-wire wire-key))
          (when (:wires rect-keys)
-           (alter selected-wires merge-selected-wire (:wires rect-keys)))
+           (alter selected-wires lel/merge-selected-wire (:wires rect-keys)))
          (alter mode dissoc :rect-x0 :rect-y0)
          )))
    KeyCode/ESCAPE (fn [_]
                     (dosync (alter mode dissoc :rect-x0 :rect-y0))
                     (release-selection))
    KeyCode/X      (fn [_] (dosync
-                            (alter lels remove-lel-by-key @selected-lels)
-                            (alter wires remove-wire-by-key @selected-wires)
+                            (alter lels lel/remove-lel-by-key @selected-lels)
+                            (alter wires lel/remove-wire-by-key @selected-wires)
                             (ref-set selected-lels #{})
                             ))})
 
@@ -774,7 +610,7 @@
      (dosync
        (alter lels conj
               {(gensym)
-               (conj (lel-init (:type @mode))
+               (conj (lel/lel-init (:type @mode))
                      @cursor-pos)})))
    })
 
@@ -834,28 +670,6 @@
 ; schematic pane
 ;--------------------------------------------------
 
-(defn jump-amount [dir]
-  (let [[fil pick move-dir]
-          (case dir
-            :left  [#(< % (:x @cursor-pos)) #(apply max %) :x]
-            :right [#(< (:x @cursor-pos) %) #(apply min %) :x]
-            :up    [#(< % (:y @cursor-pos)) #(apply max %) :y]
-            :down  [#(< (:y @cursor-pos) %) #(apply min %) :y])
-        [lelc0 lelc1 wirec0 wirec1]
-          (case move-dir
-            :x [lel-x-min lel-x-max :x0 :x1]
-            :y [lel-y-min lel-y-max :y0 :y1]
-            nil)
-        filtered (filter fil
-                         (concat (map lelc0  (vals @lels))
-                                 (map lelc1  (vals @lels))
-                                 (map wirec0 (vals @wires))
-                                 (map wirec1 (vals @wires))))]
-    (if (empty? filtered)
-      0
-      (- (pick filtered) (move-dir @cursor-pos))
-      )))
-
 (defn pane-schem-cursor-speed [keyEvent]
   (let [num ({KeyCode/DIGIT0 0, KeyCode/DIGIT1 1, KeyCode/DIGIT2 2,
               KeyCode/DIGIT3 3, KeyCode/DIGIT4 4, KeyCode/DIGIT5 5,
@@ -876,13 +690,14 @@
                   (#{KeyCode/DOWN  KeyCode/J} kc) :down
                   :else                           nil)
         speed (cond (not dir)            1
-                    (<= @cursor-speed 0) (jump-amount dir)
+                    (<= @cursor-speed 0)
+                      (lel/jump-amount dir @cursor-pos @lels @wires)
                     ('#{:left :up} dir)  (- @cursor-speed)
                     :else                @cursor-speed)
         op (case (:mode @mode)
              (:cursor :add :wire) #(move-cursor % speed)
              :move                #(do (move-cursor   % speed)
-                                      (move-selected % speed))
+                                       (move-selected % speed))
              :catalog             #(move-catalog % (case dir (:left :up) -1 1))
              nil)]
     (when (and dir op)
@@ -902,7 +717,7 @@
 (defn pane-schem-goto-dialog [keyEvent pane f-set-to-parent]
   (when (and (= KeyCode/D (.getCode keyEvent))
              (= (:mode @mode) :cursor))
-    (let [lel-key (find-lel-by-pos @lels @cursor-pos)]
+    (let [lel-key (lel/find-lel-by-pos @lels @cursor-pos)]
       (when-let [dt (dialog-table (get-in @lels [lel-key :type]))]
         (.setFocusTraversable pane false)
         (dosync (ref-set selected-name lel-key))
