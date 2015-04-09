2423fe04d94a934199d7ffa6c9661181300f6ea1
unknown <tyam@N34125.office.amsiag.com>
2015-04-09 11:47:28 +0200

    - Updated.

--- 0180_refactoring/tests/TestLogicElements.clj
+++ 0180_refactoring/tests/TestLogicElements.clj
@@ -3,30 +3,28 @@
 (require 'LogicElements)
 (alias 'dut 'LogicElements)
 
+;--------------------------------------------------
+(println "tests rectangular-select")
+
 (def lels '{g-000 {:type :dff :x 10 :y 10 :width 4 :height 5}
             g-001 {:type :and :x 20 :y 10 :width 4 :height 4}})
 (def wires '{g-100 {:x0 10 :y0 20 :x1 20 :y1 20}
              g-101 {:x0 10 :y0 20 :x1 10 :y1 30}})
 
-(def test-patts [[(dut/rectangular-select lels wires
-                   {:x 10 :y 10} {:x 14 :y 15})
-                  {:lels #{'g-000} :geoms {}}]
-                 [(dut/rectangular-select lels wires
-                   {:x 10 :y 10} {:x 13 :y 15})
-                  {:lels #{} :geoms {}}]
-                 [(dut/rectangular-select lels wires
-                   {:x 10 :y 10} {:x 14 :y 14})
-                  {:lels #{} :geoms {}}]
-                 [(dut/rectangular-select lels wires
-                   {:x 20 :y 10} {:x 24 :y 14})
-                  {:lels #{'g-001} :geoms {}}]
-                 [(dut/rectangular-select lels wires
-                   {:x 10 :y 10} {:x 24 :y 15})
-                  {:lels #{'g-000 'g-001} :geoms {}}]
-                 [(dut/rectangular-select lels wires
-                   {:x 10 :y 20} {:x 20 :y 30})
-                  {:lels #{} :geoms '{g-100 #{:p0 :p1} g-101 #{:p0 :p1}}}
-                  ]])
+(def test-patts
+  [[(dut/rectangular-select lels wires {:x 10 :y 10} {:x 14 :y 15})
+    {:lels #{'g-000} :geoms {}}]
+   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 13 :y 15})
+    {:lels #{} :geoms {}}]
+   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 14 :y 14})
+    {:lels #{} :geoms {}}]
+   [(dut/rectangular-select lels wires {:x 20 :y 10} {:x 24 :y 14})
+    {:lels #{'g-001} :geoms {}}]
+   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 24 :y 15})
+    {:lels #{'g-000 'g-001} :geoms {}}]
+   [(dut/rectangular-select lels wires {:x 10 :y 20} {:x 20 :y 30})
+    {:lels #{} :geoms '{g-100 #{:p0 :p1} g-101 #{:p0 :p1}}}
+    ]])
 
 (doseq [[result expected] test-patts]
   (if (= result expected)
@@ -37,3 +35,23 @@
   (print " result: ")
   (println result))
 
+;--------------------------------------------------
+(println "tests move-cursor")
+
+(def schem {:cursor-pos {:x 10 :y 20}})
+
+(def test-patts
+  [[(dut/move-cursor schem :x  2) {:cursor-pos {:x 12 :y 20}}]
+   [(dut/move-cursor schem :x -2) {:cursor-pos {:x  8 :y 20}}]
+   [(dut/move-cursor schem :y  3) {:cursor-pos {:x 10 :y 23}}]
+   [(dut/move-cursor schem :y -3) {:cursor-pos {:x 10 :y 17}}]
+   ])
+
+(doseq [[result expected] test-patts]
+  (if (= result expected)
+    (print "[OK]")
+    (do
+      (print "[ER] expected: ")
+      (print expected)))
+  (print " result: ")
+  (println result))
