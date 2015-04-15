9ca1588d87e91187cfdbc149936394ad5ee84ddc
unknown <tyam@N34125.office.amsiag.com>
2015-04-15 05:00:20 +0200

    - Updated.

--- 0180_refactoring/tests/TestLogicElements.clj
+++ 0180_refactoring/tests/TestLogicElements.clj
@@ -8,24 +8,24 @@
 ;--------------------------------------------------
 (println "tests rectangular-select")
 
-(def lels '{g-000 {:type :dff :x 10 :y 10 :width 4 :height 5}
-            g-001 {:type :and :x 20 :y 10 :width 4 :height 4}})
-(def wires '{g-100 {:x0 10 :y0 20 :x1 20 :y1 20}
-             g-101 {:x0 10 :y0 20 :x1 10 :y1 30}})
+(def lels '{g-000 {:type :dff :p [10 10] :width 4 :height 5}
+            g-001 {:type :and :p [20 10] :width 4 :height 4}})
+(def wires '{g-100 {:p [[10 20] [20 20]]}
+             g-101 {:p [[10 20] [10 30]]}})
 
 (def test-patts
-  [[(dut/rectangular-select lels wires {:x 10 :y 10} {:x 14 :y 15})
+  [[(dut/rectangular-select lels wires [10 10] [14 15])
     {:lels #{'g-000} :geoms {}}]
-   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 13 :y 15})
+   [(dut/rectangular-select lels wires [10 10] [13 15])
     {:lels #{} :geoms {}}]
-   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 14 :y 14})
+   [(dut/rectangular-select lels wires [10 10] [14 14])
     {:lels #{} :geoms {}}]
-   [(dut/rectangular-select lels wires {:x 20 :y 10} {:x 24 :y 14})
+   [(dut/rectangular-select lels wires [20 10] [24 14])
     {:lels #{'g-001} :geoms {}}]
-   [(dut/rectangular-select lels wires {:x 10 :y 10} {:x 24 :y 15})
+   [(dut/rectangular-select lels wires [10 10] [24 15])
     {:lels #{'g-000 'g-001} :geoms {}}]
-   [(dut/rectangular-select lels wires {:x 10 :y 20} {:x 20 :y 30})
-    {:lels #{} :geoms '{g-100 #{:p0 :p1} g-101 #{:p0 :p1}}}
+   [(dut/rectangular-select lels wires [10 20] [20 30])
+    {:lels #{} :geoms '{g-100 #{0 1} g-101 #{0 1}}}
     ]])
 
 (doseq [[result expected] test-patts]
@@ -40,13 +40,13 @@
 ;--------------------------------------------------
 (println "tests move-cursor")
 
-(def schem {:cursor-pos {:x 10 :y 20}})
+(def schem {:cursor-pos [10 20]})
 
 (def test-patts
-  [[(dut/move-cursor schem :x  2) {:cursor-pos {:x 12 :y 20}}]
-   [(dut/move-cursor schem :x -2) {:cursor-pos {:x  8 :y 20}}]
-   [(dut/move-cursor schem :y  3) {:cursor-pos {:x 10 :y 23}}]
-   [(dut/move-cursor schem :y -3) {:cursor-pos {:x 10 :y 17}}]
+  [[(dut/move-cursor schem [ 2  0]) {:cursor-pos [12 20]}]
+   [(dut/move-cursor schem [-2  0]) {:cursor-pos [ 8 20]}]
+   [(dut/move-cursor schem [ 0  3]) {:cursor-pos [10 23]}]
+   [(dut/move-cursor schem [ 0 -3]) {:cursor-pos [10 17]}]
    ])
 
 (doseq [[result expected] test-patts]
@@ -62,37 +62,37 @@
 (println "tests move-selected")
 
 (def schem
-  {:moving-lels  {'G0 {:x   0 :y   1}
-                  'G1 {:x  10 :y  11}}
-   :moving-geoms {'G2 {:x0 20 :y0 21 :x1 22 :y1 23}
-                  'G3 {:x0 30 :y0 31 :x1 32 :y1 33}}
-   :geoms        {'G4 {:x0 40 :y0 41 :x1 42 :y1 43}
-                  'G5 {:x0 50 :y0 51 :x1 52 :y1 53}}
-   :moving-vertices {'G4 #{':p0}
-                     'G5 #{':p1}
+  {:moving-lels  {'G0 {:p [ 0  1]}
+                  'G1 {:p [10 11]}}
+   :moving-geoms {'G2 {:p [[20 21] [22 23]]}
+                  'G3 {:p [[30 31] [32 33]]}}
+   :geoms        {'G4 {:p [[40 41] [42 43]]}
+                  'G5 {:p [[50 51] [52 53]]}}
+   :moving-vertices {'G4 #{0}
+                     'G5 #{1}
                      }})
 
 (def test-patts
-  [[(dut/move-selected schem :x 2)
+  [[(dut/move-selected schem [2 0])
     (-> schem
-        (update-in [:moving-lels  'G0 :x ] #(+ 2 %))
-        (update-in [:moving-lels  'G1 :x ] #(+ 2 %))
-        (update-in [:moving-geoms 'G2 :x0] #(+ 2 %))
-        (update-in [:moving-geoms 'G2 :x1] #(+ 2 %))
-        (update-in [:moving-geoms 'G3 :x0] #(+ 2 %))
-        (update-in [:moving-geoms 'G3 :x1] #(+ 2 %))
-        (update-in [:geoms        'G4 :x0] #(+ 2 %))
-        (update-in [:geoms        'G5 :x1] #(+ 2 %)))]
-   [(dut/move-selected schem :y -3)
+        (update-in [:moving-lels  'G0 :p  ] #(vec (map + [2 0] %)))
+        (update-in [:moving-lels  'G1 :p  ] #(vec (map + [2 0] %)))
+        (update-in [:moving-geoms 'G2 :p 0] #(vec (map + [2 0] %)))
+        (update-in [:moving-geoms 'G2 :p 1] #(vec (map + [2 0] %)))
+        (update-in [:moving-geoms 'G3 :p 0] #(vec (map + [2 0] %)))
+        (update-in [:moving-geoms 'G3 :p 1] #(vec (map + [2 0] %)))
+        (update-in [:geoms        'G4 :p 0] #(vec (map + [2 0] %)))
+        (update-in [:geoms        'G5 :p 1] #(vec (map + [2 0] %))))]
+   [(dut/move-selected schem [0 -3])
     (-> schem
-        (update-in [:moving-lels  'G0 :y ] #(- % 3))
-        (update-in [:moving-lels  'G1 :y ] #(- % 3))
-        (update-in [:moving-geoms 'G2 :y0] #(- % 3))
-        (update-in [:moving-geoms 'G2 :y1] #(- % 3))
-        (update-in [:moving-geoms 'G3 :y0] #(- % 3))
-        (update-in [:moving-geoms 'G3 :y1] #(- % 3))
-        (update-in [:geoms        'G4 :y0] #(- % 3))
-        (update-in [:geoms        'G5 :y1] #(- % 3))
+        (update-in [:moving-lels  'G0 :p  ] #(vec (map + % [0 -3])))
+        (update-in [:moving-lels  'G1 :p  ] #(vec (map + % [0 -3])))
+        (update-in [:moving-geoms 'G2 :p 0] #(vec (map + % [0 -3])))
+        (update-in [:moving-geoms 'G2 :p 1] #(vec (map + % [0 -3])))
+        (update-in [:moving-geoms 'G3 :p 0] #(vec (map + % [0 -3])))
+        (update-in [:moving-geoms 'G3 :p 1] #(vec (map + % [0 -3])))
+        (update-in [:geoms        'G4 :p 0] #(vec (map + % [0 -3])))
+        (update-in [:geoms        'G5 :p 1] #(vec (map + % [0 -3])))
         )]])
 
 (doseq [[result expected] test-patts]
@@ -112,7 +112,7 @@
    :lels  {'G0 :g0 'G1 :g1}
    :geoms {'G2 :g2 'G3 :g3 'G4 :g4 'G5 :g5}
    :selected-lels #{'G0}
-   :selected-geoms {'G2 #{:p0 :p1} 'G3 #{:p0} 'G4 #{:p1}}
+   :selected-geoms {'G2 #{0 1} 'G3 #{0} 'G4 #{1}}
    })
 (def schem-move-mode
   {:mode :move
@@ -120,7 +120,7 @@
    :geoms {'G3 :g3 'G4 :g4 'G5 :g5}
    :moving-lels {'G0 :g0}
    :moving-geoms {'G2 :g2}
-   :moving-vertices {'G3 #{:p0} 'G4 #{:p1}}
+   :moving-vertices {'G3 #{0} 'G4 #{1}}
    :revert-schem schem
    })
 (def test-patts
@@ -156,7 +156,7 @@
    :lels  {'G0 :g0 'G1 :g1}
    :geoms {'G2 :g2 'G3 :g3 'G4 :g4 'G5 :g5}
    :selected-lels #{'G0}
-   :selected-geoms {'G2 #{:p0 :p1} 'G3 #{:p0} 'G4 #{:p1}}
+   :selected-geoms {'G2 #{0 1} 'G3 #{0} 'G4 #{1}}
    })
 (def schem-copy-mode
   {:mode :copy
