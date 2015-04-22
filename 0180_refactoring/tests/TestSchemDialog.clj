182be706c3ebbae90d23f3e0a5445a5153618a81
unknown <tyam@N34125.office.amsiag.com>
2015-04-22 05:00:30 +0200

    - Updated.

--- 0180_refactoring/tests/TestSchemDialog.clj
+++ 0180_refactoring/tests/TestSchemDialog.clj
@@ -21,23 +21,25 @@
 
 (defn auto-key-events [stage label]
   (Thread/sleep 500)
-  (doseq [e (map #(KeyEvent. nil (% 0) KeyEvent/KEY_PRESSED (% 1) (% 1) (% 2)
-                   (% 3) false false false) ; shift ctrl alt meta
-                 [[label  "d"  KeyCode/D     false]
-                  [nil    "l"  KeyCode/L     false]
-                  [nil    "j"  KeyCode/J     false]
-                  [nil    "l"  KeyCode/L     false]
-                  [nil    "l"  KeyCode/L     false]
-                  [nil    "j"  KeyCode/J     false]
-                  [nil    " "  KeyCode/SPACE false]
-                  [nil    "f"  KeyCode/F     false]
-                  [nil    "o"  KeyCode/O     false]
-                  [nil    "o"  KeyCode/O     false]
-                  [nil    "\n" KeyCode/ENTER true ]
-                  [nil    "\n" KeyCode/ENTER false]])]
-    (Thread/sleep 500)
-    (print "Fires ") (println (.toString e))
-    (.fireEvent stage e)))
+  (.fireEvent label (KeyEvent. KeyEvent/KEY_PRESSED "d" "d" KeyCode/D
+                     false false false false)))
+  ;(doseq [e (map #(KeyEvent. nil (% 0) KeyEvent/KEY_PRESSED (% 1) (% 1) (% 2)
+  ;                 (% 3) false false false) ; shift ctrl alt meta
+  ;               [[label  "d"  KeyCode/D     false]
+  ;                [nil    "l"  KeyCode/L     false]
+  ;                [nil    "j"  KeyCode/J     false]
+  ;                [nil    "l"  KeyCode/L     false]
+  ;                [nil    "l"  KeyCode/L     false]
+  ;                [nil    "j"  KeyCode/J     false]
+  ;                [nil    " "  KeyCode/SPACE false]
+  ;                [nil    "f"  KeyCode/F     false]
+  ;                [nil    "o"  KeyCode/O     false]
+  ;                [nil    "o"  KeyCode/O     false]
+  ;                [nil    "\n" KeyCode/ENTER true ]
+  ;                [nil    "\n" KeyCode/ENTER false]])]
+  ;  (Thread/sleep 500)
+  ;  (print "Fires ") (println (.toString e))
+  ;  (.fireEvent stage e)))
 
 (defn label-revert [f-set-to-parent label]
   (f-set-to-parent label)
