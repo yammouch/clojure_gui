deefc29bb6d2cbe541aafa14ee519819f8fb994f
Takuya Yamauchi <yammouch@gmail.com>
2015-05-10 20:33:28 +0900

    - Updated.

--- 0180_refactoring/tests/run_all_tests.clj
+++ 0180_refactoring/tests/run_all_tests.clj
@@ -10,4 +10,19 @@
   (println (:out result))
   (println (:err result)))
 
+(let [result (sh "java" "-cp" "..;../dut;../../../clojure-1.6.0.jar"
+                 "clojure.main" "-i" "compile_tests.clj")]
+  (println (:out result))
+  (println (:err result)))
+
+(let [result (sh "java" "-cp" "./classes;../../../clojure-1.6.0.jar"
+                 "tests.TestSchemDialog")]
+  (println (:out result))
+  (println (:err result)))
+
+(let [result (sh "java" "-cp" "./classes;../../../clojure-1.6.0.jar"
+                 "tests.TestLogicElementsDraw")]
+  (println (:out result))
+  (println (:err result)))
+
 (System/exit 0)
