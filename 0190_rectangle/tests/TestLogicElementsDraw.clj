ea52ad24c347a07f4cc4c940f8b081924b2d890f
unknown <tyam@N34125.office.amsiag.com>
2015-05-14 14:17:34 +0200

    - Compile passes.

--- 0190_rectangle/tests/TestLogicElementsDraw.clj
+++ 0190_rectangle/tests/TestLogicElementsDraw.clj
@@ -26,7 +26,9 @@
    :geoms {'G_100 {:type :wire :p [[1 5] [5 5]]}
            'G_101 {:type :wire :p [[1 6] [5 6]]}
            'G_102 {:type :wire :p [[1 7] [5 7]]}
-           'G_103 {:type :wire :p [[1 8] [5 8]]}}
+           'G_103 {:type :wire :p [[1 8] [5 8]]}
+           'G_104 {:type :rect :p [[15 1] [20  5]]}
+           'G_105 {:type :rect :p [[15 6] [20 10]]}}
    :rect-p0 [10 9]
    :selected-lels  #{'G_002}
    :selected-geoms {'G_100 #{0 1}
@@ -35,13 +37,16 @@
                     }})
 
 (def schem-2 (lel/move-mode schem-1))
+(def schem-3 (lel/move-mode schem-1 true))
+(def schem-4 (lel/add-mode  schem-1 :and))
 
 (defn -start [_ stage]
-  (let [pane1 (Pane.)
-        pane2 (Pane.)
+  (let [pane1 (Pane.) pane2 (Pane.) pane3 (Pane.) pane4 (Pane.)
         gridpane (GridPane.)]
     (.add gridpane pane1 0 0)
     (.add gridpane pane2 1 0)
+    (.add gridpane pane3 0 1)
+    (.add gridpane pane4 1 1)
     (doto stage
       (.setWidth 640) (.setHeight 480)
       (.setScene (Scene. gridpane))
@@ -49,6 +54,8 @@
       (.show))
     (.setAll (.getChildren pane1) (ld/draw-mode schem-1))
     (.setAll (.getChildren pane2) (ld/draw-mode schem-2))
+    (.setAll (.getChildren pane3) (ld/draw-mode schem-3))
+    (.setAll (.getChildren pane4) (ld/draw-mode schem-4))
     ))
 
 (defn -main [& args]
