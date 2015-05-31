65d6aaed8db85d82a3dffe988f91db7ea1b4a3de
Takuya Yamauchi <yammouch@gmail.com>
2015-05-31 19:52:46 +0900

    - Updated.

--- 0200_javafx/canvas_size_bind/compile.clj
+++ 0200_javafx/canvas_size_bind/compile.clj
@@ -1,10 +1,10 @@
 ; do following
 ; $ java -cp .:/path/to/clojure-1.6.0.jar clojure.main -i compile.clj
-; $ java -cp ./classes:/path/to/clojure-1.6.0.jar canvas_size_show
+; $ java -cp ./classes:/path/to/clojure-1.6.0.jar canvas_size_bind
 ; (for DOS ...)
 ; > java -cp .;\path\to\clojure-1.6.0.jar clojure.main -i compile.clj
-; > java -cp classes;\path\to\clojure-1.6.0.jar canvas_size_show
+; > java -cp classes;\path\to\clojure-1.6.0.jar canvas_size_bind
 
 (def force-toolkit-init (javafx.embed.swing.JFXPanel.))
-(compile 'canvas_size_show)
+(compile 'canvas_size_bind)
 (System/exit 0)
