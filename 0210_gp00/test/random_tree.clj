06a83dce9d54c44cd1794dee29ad560f632fe8bc
Takuya Yamauchi <yammouch@gmail.com>
2015-07-04 21:08:19 +0900

    - Updated.

--- 0210_gp00/test/random_tree.clj
+++ 0210_gp00/test/random_tree.clj
@@ -3,12 +3,13 @@
 (require 'random-tree)
 (alias 'rtr 'random-tree)
 
-(def test-cases [[8 3] [15 3] [1 3] [3 1] [3 15]])
+(def test-cases [[8 3] [15 3] [1 3] [3 1] [3 15] [3 0]])
 
 (doseq [x test-cases]
   (let [result (apply rtr/distribute x)]
     (if (and (= (count result) (x 1))
-             (= (reduce + result) (x 0)))
+             (or (= (reduce + result) (x 0))
+                 (= (x 1) 0)))
       (print "[OK]")
       (print "[ER]"))
     (print " test case ")
