2eef74f7ea4cdb77cbf1cf140ed6bf0f8d3d585b
unknown <tyam@N34125.office.amsiag.com>
2015-07-06 12:07:07 +0200

    - First checkin.

---
+++ 0210_gp00/test/roulette.clj
@@ -0,0 +1,21 @@
+(ns test.roulette)
+
+(require 'roulette)
+(alias 'rlt 'roulette)
+
+(def test-cases
+ [{:in [[1 :a] [3 :b] [6 :c]] :exp [10 [[1 :a] [4 :b] [10 :c]]]}
+  {:in [[1 :a] [1 :b] [1 :c]] :exp [ 3 [[1 :a] [2 :b] [ 3 :c]]]}
+  {:in [[0 :a] [0 :b] [1 :c]] :exp [ 1 [[0 :a] [0 :b] [ 1 :c]]]}])
+  
+(doseq [x test-cases]
+  (let [result (rlt/sumup (:in x))]
+    (if (= (:exp x) result)
+      (print "[OK]")
+      (print "[ER]"))
+    (print " test case ")
+    (println (:in x))))
+
+(let [f (rlt/make-roulette-wheel-selector (get-in test-cases 0 :in))]
+  (println (->> (map (fn [_] (f)) (repeat 100 nil))
+                frequency sort)))
