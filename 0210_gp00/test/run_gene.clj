c7076789d925453e0ce58d4b3e888934b096b6eb
Takuya Yamauchi <yammouch@gmail.com>
2015-07-19 12:31:22 +0900

    - Tests for 'gene-op' have been added.

--- 0210_gp00/test/run_gene.clj
+++ 0210_gp00/test/run_gene.clj
@@ -3,53 +3,31 @@
 (require 'run-gene)
 (alias 'rg 'run-gene)
 
-(defn test-2-terms-op [f test-cases]
-  (doseq [[[x y] exp] test-cases]
-    (let [[result _] (f [x y] nil)]
-      (if (= exp result)
-        (print "[OK]")
-        (print "[ER]" result))
-      (print " test case ")
-      (println [x y]))))
-
-(println "test gene-+")
-(test-2-terms-op rg/gene-+
-                 [[[32000 767]  32767]
-                  [[32000 768] -32768]
-                  [[32000 769] -32767]])
-
-(println "test gene--")
-(test-2-terms-op rg/gene--
-                 [[[-1 32766] -32767]
-                  [[-1 32767] -32768]
-                  [[-1 32768]  32767]])
+(println "test gene-op")
+(def test-cases [[[0  32767 32768]      0]
+                 [[0  32768 32767]      1]
+                 [[1  32767 32768]      1]
+                 [[1  32768 32767]      0]
+                 [[2  32767 32768]      0]
+                 [[2  65537     1]      1]
+                 [[3  32000   767]  32767]
+                 [[3  32000   768] -32768]
+                 [[3  32000   769] -32767]
+                 [[4     -1 32766] -32767]
+                 [[4     -1 32767] -32768]
+                 [[4     -1 32768]  32767]
+                 [[5    256   256]      0]
+                 [[5    255   256]   -256]
+                 [[6    256   256]      1]
+                 [[6 131072     2]      0]])
   
-(println "test gene-*")
-(test-2-terms-op rg/gene-*
-                 [[[256 256]    0]
-                  [[255 256] -256]])
-  
-(println "test gene-div")
-(test-2-terms-op rg/gene-div
-                 [[[   256 256] 1]
-                  [[131072   2] 0]])
-  
-(println "test gene-<")
-(test-2-terms-op rg/gene-<
-                 [[[32767 32768] 0]
-                  [[32768 32767] 1]])
-  
-(println "test gene->")
-(test-2-terms-op rg/gene->
-                 [[[32767 32768] 1]
-                  [[32768 32767] 0]])
-
-(println "test gene-=")
-(test-2-terms-op rg/gene-=
-                 [[[32767 32768] 0]
-                  [[65537     1] 1]])
-
-(println "test gene-pos")
+(doseq [[[op x y] exp] test-cases]
+  (let [[result _] (rg/gene-op [op x y] nil)]
+    (if (= exp result)
+      (print "[OK]")
+      (print "[ER]" result))
+    (print " test case ")
+    (println [op x y])))
 
 (def env-1
   {:nodes [[-5 -4]
@@ -162,11 +140,11 @@
        :edges [[0 1] [1 2]]
        }]]])
 
-(doseq [[gene exp] test-cases]
-  (let [result (rg/eval-gene gene env-1)]
-    (if (= exp result)
-      (print "[OK]")
-      (print "[ER]" result))
-    (print " test case ")
-    (println gene)))
+;(doseq [[gene exp] test-cases]
+;  (let [result (rg/eval-gene gene env-1)]
+;    (if (= exp result)
+;      (print "[OK]")
+;      (print "[ER]" result))
+;    (print " test case ")
+;    (println gene)))
 
