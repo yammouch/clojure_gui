07916d0153e7c35801b5d115f28706f962debb67
Takuya Yamauchi <yammouch@gmail.com>
2015-07-20 22:17:34 +0900

    - First checkin.

--- 0210_gp00/dut/gp_main.clj
+++ 0210_gp00/dut/gp_main.clj
@@ -5,6 +5,8 @@
 (use '[run-gene    :as rg])
 (use '[random-tree :as rt])
 
+(def rand-obj (java.util.Random. 1))
+
 (def pool-size 2000)
 (def gene-size-limit 100)
 (def mutation-prob 0.01)
@@ -13,5 +15,19 @@
   (map (fn [_] (rt:gen-tree 6))
        (range pool-size)))
 
-;(defn score-gene [])
+;(defn score-genes [pool])
 
+(defn mutate [gene]
+  (let [nn (hg/count-node gene)
+        mutant-src (rt/gen-tree 6)]
+    (hg/replace-node gene (.nextInt rand-obj nn) mutant-src)))
+
+(defn next-pool [scored-pool]
+  (loop [roulette (rl/make-roulette-wheel-selector scored-pool
+         num 0 retval []]
+    (let [mother (roulette) mother-nn (count-mode mother)
+          father (roulette) father-nn (count-node father)
+          child (hg/crossover mother father
+                              (.nextInt rand-obj mother-nn)
+                              (.nextInt rand-obj father-nn))
+          child ()]
