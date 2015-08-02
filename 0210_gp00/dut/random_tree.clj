10f82825dc52c25a14ee0961356b598918439f49
Tackya Yammouch <yammouch@gmail.com>
2015-08-02 20:18:33 +0900

    - Updated so that uses utils/lcg.

--- 0210_gp00/dut/random_tree.clj
+++ 0210_gp00/dut/random_tree.clj
@@ -11,34 +11,37 @@
        (filter (fn [[k arity]] (< 0 arity)))
        ))
 
-(def rand-obj (java.util.Random. 1)) ; arg: seed
-
-(defn gen-terminal []
-  (let [x (.nextInt rand-obj 128)]
-    (case x
+(defn gen-terminal [rnd]
+  (let [rnd (mod rnd 128)]
+    (case rnd
       0                '(:finish)
-      (int (Math/floor (+ 0.5 (* 8 (.nextGaussian rand-obj)))))
-      )))
+      (- rnd 64))))
 
-(defn distribute [i n]
+(defn distribute [i n rnd-seq]
   "Distributes i into n-element array. i must be > 0, n must be >= 0."
-  (cond (<= n 0) []
-        (<= i 0) (repeat n 0)
+  (cond (<= n 0) [[] rnd-seq]
+        (<= i 0) [(repeat n 0) rnd-seq]
         :else
         (let [rands (sort (concat [0 i]
-                                  (map (fn [_] (.nextInt rand-obj i))
-                                       (range (dec n))
+                                  (map (fn [x] (mod x i))
+                                       (take (dec n) rnd-seq)
                                        )))]
-          (map (fn [[x y]] (- y x)) (partition 2 1 rands))
-          )))
+          [(map (fn [[x y]] (- y x)) (partition 2 1 rands))
+           (drop (dec n) rnd-seq)
+           ])))
 
-(defn gen-tree [n-nt]
+(defn gen-tree [n-nt rnd-seq]
   "n-nt is the number of non-terminal"
   (if (<= n-nt 0)
-    (gen-terminal)
+    [(gen-terminal (first rnd-seq)) (next rnd-seq)]
     (let [[nt-symbol n-children]
-          (nth non-terminals (.nextInt rand-obj (count non-terminals)))]
-      (cons nt-symbol
-            (map gen-tree
-                 (distribute (dec n-nt) n-children)
-                 )))))
+          (nth non-terminals (mod (first rnd-seq) (count non-terminals)))
+          [children rnd-seq]
+          (loop [[nc rs] (distribute (dec n-nt) n-children (next rnd-seq))
+                 acc []]
+            (if (empty? nc)
+              [acc rs]
+              (let [[child rs-next] (gen-tree (first nc) rs)]
+                (recur [(next nc) rs-next] (conj acc child))
+                )))]
+      [(cons nt-symbol children) rnd-seq])))
--- 0210_gp00/test/random_tree.clj
+++ 0210_gp00/test/random_tree.clj
@@ -2,11 +2,15 @@
 
 (require 'random-tree)
 (alias 'rtr 'random-tree)
+(require 'utils)
+(alias 'ut 'utils)
+
+(set! *print-length* 10)
 
 (def test-cases [[8 3] [15 3] [1 3] [3 1] [3 15] [3 0]])
 
 (doseq [x test-cases]
-  (let [result (apply rtr/distribute x)]
+  (let [[result _] (rtr/distribute (x 0) (x 1) (ut/lcg 0))]
     (if (and (= (count result) (x 1))
              (or (= (reduce + result) (x 0))
                  (= (x 1) 0)))
@@ -14,5 +18,5 @@
       (print "[ER]"))
     (print " test case ")
     (println x)))
-       
-(println (rtr/gen-tree 4))
+
+(println (rtr/gen-tree 4 (ut/lcg 0)))
