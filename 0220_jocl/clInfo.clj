65571b073222186c7a5fc6953d3e6db89e2e0f93
unknown <tyam@N34125.office.amsiag.com>
2015-09-11 11:59:08 +0200

    - Updated.

--- 0220_jocl/clInfo.clj
+++ 0220_jocl/clInfo.clj
@@ -24,6 +24,60 @@
     (take (nth param-value-size-ret 0) param-value-body)
     ))
 
+(def long-props '[VENDOR_ID
+                  MAX_COMPUTE_UNIT
+                  MAX_WORK_ITEM_DEMENSTIONS
+                  MAX_WORK_GROUP_SIZE
+                  PREFERRED_VECTOR_WIDTH_CHAR
+                  PREFERRED_VECTOR_WIDTH_SHORT
+                  PREFERRED_VECTOR_WIDTH_INT
+                  PREFERRED_VECTOR_WIDTH_FLOAT
+                  PREFERRED_VECTOR_WIDTH_DOUBLE
+                  MAX_CLOCK_FREQUENCY
+                  ADDRESS_BITS
+                  MAX_MEM_ALLOC_SIZE
+                  IMAGE_SUPPORT
+                  MAX_READ_IMAGE_ARGS
+                  MAX_WRITE_IMAGE_ARGS
+                  IMAGE2D_MAX_WIDTH
+                  IMAGE2D_MAX_HEIGHT
+                  IMAGE3D_MAX_WIDTH
+                  IMAGE3D_MAX_HEIGHT
+                  IMAGE3D_MAX_DEPTH
+                  MAX_SAMPLERS
+                  MAX_PARAMETER_SIZE
+                  MEM_BASE_ADDR_ALIGN
+                  MIN_DATA_TYPE_ALIGN_SIZE
+                  GLOBAL_MEM_CACHELINE_SIZE
+                  GLOBAL_MEM_CACHE_SIZE
+                  GLOBAL_MEM_SIZE
+                  MAX_CONSTANT_BUFFER_SIZE
+                  MAX_CONSTANT_ARGS
+                  LOCAL_MEM_SIZE
+                  ERROR_CORRECTION_SUPPORT
+                  PROFILING_TIMER_RESOLUTION
+                  ENDIAN_LITTLE
+                  AVAILABLE
+                  COMPILER_AVAILABLE])
+
+(def str-props '[NAME
+                 VENDOR
+                 PROFILE
+                 VERSION
+                 EXTENSIONS])
+
+(def hex-props '[SINGLE_FP_CONFIG
+                 QUEUE_PROPERTIES])
+
+
+;/* XXX For completeness, it'd be nice to dump this one, too. */
+;#define WEIRD_PROPS \
+;   CL_DEVICE_MAX_WORK_ITEM_SIZES,
+
+;(defn get-device-info [device]
+;  (let [names (map #(str "CL_DEVICE_" %) hex-props)]
+    
+
 (defn clGetPlatformIDs []
   (let [num-entries 256
         platforms (make-array org.jocl.cl_platform_id num-entries)
