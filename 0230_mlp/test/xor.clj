6d582e04536ecd3d2355c5c14b92b7b81fceac74
Takuya Yamauchi <yammouch@gmail.com>
2015-09-20 14:49:51 +0900

    - First checkin.

---
+++ 0230_mlp/test/xor.clj
@@ -0,0 +1,26 @@
+(ns xor)
+
+(require 'mlp)
+(require 'clojure.pprint)
+
+(let [training-data [[[0.0 0.0] [0.0]]
+                     [[0.0 1.0] [1.0]]
+                     [[1.0 0.0] [1.0]]
+                     [[1.0 1.0] [0.0]]
+                     ]]
+  (loop [i 0
+         weight-array [[[0.0 0.0]
+                        [0.0 0.0]]
+                       [[0.0 0.0]]]
+         b-array [[0.0 0.0] [0.0]]]
+    (if (<= 10 i)
+      :done
+      (do (println "iter:" i)
+          (clojure.pprint/pprint [weight-array b-array])
+          (let [outputs (map (fn [[in _]]
+                               (mlp/calc-output weight-array b-array in))
+                             training-data)
+                updates (map (fn [[out-array deriv-array] [_ expc]]
+                               (mlp/calc-coeff-deriv weight-array deriv-array
+                                                     out-array expc -0.025)))
+                             outputs training-data)]
