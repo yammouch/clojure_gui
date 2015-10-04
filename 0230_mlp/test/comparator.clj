244d371d9a3c43897aeef360444b08b6c98d735e
unknown <tyam@N34125.office.amsiag.com>
2015-10-04 11:23:57 +0200

    - Something goes wrong. b should converge to -1.5, but it goes to -inf.

--- 0230_mlp/test/comparator.clj
+++ 0230_mlp/test/comparator.clj
@@ -11,24 +11,26 @@
                     ])
 
 (loop [i 0
+       td (cycle training-data)
        weight-array [[[0.0]]]
        b-array [[0.0]] ]
-  (if (<= 10 i)
+  (if (<= 10000 i)
     :done
     (let [outputs (map (fn [[in _]]
                          (mlp/calc-output weight-array b-array in))
-                       training-data)
+                       (take 1 training-data))
           updates (map (fn [[out-array deriv-array] [_ expc]]
                          (mlp/calc-coeff-deriv weight-array deriv-array
                                                out-array expc -0.025))
-                       outputs training-data)]
+                       outputs (take 1 training-data))]
       (println "iter:" i)
-      (clojure.pprint/pprint outputs)
-      (clojure.pprint/pprint updates)
+      ;(clojure.pprint/pprint outputs)
+      ;(clojure.pprint/pprint updates)
       (clojure.pprint/pprint [weight-array b-array])
-      (clojure.pprint/pprint (first (first updates)))
-      (clojure.pprint/pprint (mlp/r+ weight-array (first (first updates))))
+      ;(clojure.pprint/pprint (first (first updates)))
+      ;(clojure.pprint/pprint (mlp/r+ weight-array (first (first updates))))
       (recur (inc i)
+             (drop 1 training-data)
              (apply mlp/r+ weight-array (map first updates))
              (apply mlp/r+ b-array (map second updates))
              ))))
