08edd00a608c38172e44adf69b84986bb656c464
unknown <tyam@N34125.office.amsiag.com>
2015-10-04 10:12:36 +0200

    - Now something outputs.

--- 0230_mlp/test/xor.clj
+++ 0230_mlp/test/xor.clj
@@ -3,28 +3,34 @@
 (require 'mlp)
 (require 'clojure.pprint)
 
-(let [training-data [[[0.0 0.0] [0.0]]
-                     [[0.0 1.0] [1.0]]
-                     [[1.0 0.0] [1.0]]
-                     [[1.0 1.0] [0.0]]
-                     ]]
-  (loop [i 0
-         weight-array [[[0.0 0.0]
-                        [0.0 0.0]]
-                       [[0.0 0.0]]]
-         b-array [[0.0 0.0] [0.0]]]
-    (if (<= 10 i)
-      :done
-      (let [outputs (map (fn [[in _]]
-                           (mlp/calc-output weight-array b-array in))
-                         training-data)
-            updates (map (fn [[out-array deriv-array] [_ expc]]
-                           (mlp/calc-coeff-deriv weight-array deriv-array
-                                                 out-array expc -0.025))
-                         outputs training-data)]
-        (println "iter:" i)
-        (clojure.pprint/pprint [weight-array b-array])
-        (recur (inc i)
-               (apply mlp/m+ weight-array (map first updates))
-               (apply map + b-array (map second updates))
-               )))))
+
+(def training-data [[[0.0 0.0] [0.0]]
+                    [[0.0 1.0] [1.0]]
+                    [[1.0 0.0] [1.0]]
+                    [[1.0 1.0] [0.0]]
+                    ])
+
+(loop [i 0
+       weight-array [[[0.0 0.0]
+                      [0.0 0.0]]
+                     [[0.0 0.0]]]
+       b-array [[0.0 0.0] [0.0]]]
+  (if (<= 10 i)
+    :done
+    (let [outputs (map (fn [[in _]]
+                         (mlp/calc-output weight-array b-array in))
+                       training-data)
+          updates (map (fn [[out-array deriv-array] [_ expc]]
+                         (mlp/calc-coeff-deriv weight-array deriv-array
+                                               out-array expc -0.025))
+                       outputs training-data)]
+      (println "iter:" i)
+      (clojure.pprint/pprint outputs)
+      (clojure.pprint/pprint updates)
+      (clojure.pprint/pprint [weight-array b-array])
+      (clojure.pprint/pprint (first (first updates)))
+      (clojure.pprint/pprint (mlp/r+ weight-array (first (first updates))))
+      (recur (inc i)
+             (apply mlp/r+ weight-array (map first updates))
+             (apply mlp/r+ b-array (map second updates))
+             ))))
