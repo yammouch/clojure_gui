96ef1180eeccf11ef31c963bea05567acb4d127b
Tackya Yammouch <yammouch@gmail.com>
2015-11-01 17:09:32 +0900

    - Updated.

--- 0230_mlp/test/comparator.clj
+++ 0230_mlp/test/comparator.clj
@@ -10,6 +10,17 @@
                     [[3.0] [1.0]]
                     ])
 
+;(def weight-array [[[0.0]]])
+;(def b-array [[0.0]])
+;
+;(def outputs (map (fn [[in _]]
+;                    (mlp/calc-output weight-array b-array in))
+;                  training-data))
+;(def updates (map (fn [[out-array deriv-array] [_ expc]]
+;                     (mlp/calc-coeff-deriv weight-array deriv-array
+;                                           out-array expc -0.025))
+;                  outputs training-data))
+
 (loop [i 0
        td (cycle training-data)
        weight-array [[[0.0]]]
@@ -18,19 +29,20 @@
     :done
     (let [outputs (map (fn [[in _]]
                          (mlp/calc-output weight-array b-array in))
-                       (take 1 training-data))
+                       (take 4 td))
           updates (map (fn [[out-array deriv-array] [_ expc]]
                          (mlp/calc-coeff-deriv weight-array deriv-array
                                                out-array expc -0.025))
-                       outputs (take 1 training-data))]
+                       outputs (take 4 td))]
       (println "iter:" i)
-      ;(clojure.pprint/pprint outputs)
-      ;(clojure.pprint/pprint updates)
+      (clojure.pprint/pprint outputs)
+      (clojure.pprint/pprint updates)
       (clojure.pprint/pprint [weight-array b-array])
       ;(clojure.pprint/pprint (first (first updates)))
       ;(clojure.pprint/pprint (mlp/r+ weight-array (first (first updates))))
+      ;(read)
       (recur (inc i)
-             (drop 1 training-data)
+             (drop 4 td)
              (apply mlp/r+ weight-array (map first updates))
              (apply mlp/r+ b-array (map second updates))
              ))))
