db5c7d8cfe788d2556b26b572642ca8a33688211
unknown <tyam@N34125.office.amsiag.com>
2015-12-04 06:34:52 +0100

    - Updated.

--- 0220_jocl/cl.clj
+++ 0220_jocl/cl.clj
@@ -1,13 +1,17 @@
 (ns cl)
 
-; wrapper of OpenCL API
+; very thin wrapper of OpenCL API
 
 (defn clGetPlatformIDs []
   (let [num-entries 256
         platforms (make-array org.jocl.cl_platform_id num-entries)
-        num-platforms (int-array 1)]
-    (org.jocl.CL/clGetPlatformIDs num-entries platforms num-platforms)
-    (take (nth num-platforms 0) platforms)))
+        num-platforms (int-array 1)
+        errcode-ret (org.jocl.CL/clGetPlatformIDs
+                     num-entries platforms num-platforms)]
+    (if (= errcode-ret org.jocl.CL/CL_SUCCESS)
+      (take (nth num-platforms 0) platforms)
+      (throw (Exception. (org.jocl.CL/stringFor_errorCode errcode-ret)))
+      )))
 
 (defn clGetPlatformInfo [platform param-name]
   (let [param-value-size 65536
@@ -21,8 +25,11 @@
      param-value-size
      param-value
      param-value-size-ret)
-    (take (nth param-value-size-ret 0)
-          param-value-body)))
+    (if (= (nth errcode-ret 0) org.jocl.CL/CL_SUCCESS)
+      (take (nth param-value-size-ret 0)
+            param-value-body)
+      (throw (Exception. (org.jocl.CL/stringFor_errorCode errcode-ret)))
+      )))
 
 (defn clGetDeviceIDs [platform]
   (let [num-devices (int-array 1)
@@ -47,3 +54,108 @@
      param-value-size-ret)
     (take (nth param-value-size-ret 0) param-value-body)
     ))
+
+; subroutines for get bunch of OpenCL infomation
+
+(def long-props (map #(symbol (str "CL_DEVICE_" %))
+                '[VENDOR_ID
+                  MAX_COMPUTE_UNITS
+                  MAX_WORK_ITEM_DIMENSIONS
+                  MAX_WORK_GROUP_SIZE
+                  PREFERRED_VECTOR_WIDTH_CHAR
+                  PREFERRED_VECTOR_WIDTH_SHORT
+                  PREFERRED_VECTOR_WIDTH_INT
+                  PREFERRED_VECTOR_WIDTH_FLOAT
+                  PREFERRED_VECTOR_WIDTH_DOUBLE
+                  MAX_CLOCK_FREQUENCY
+                  ADDRESS_BITS
+                  MAX_MEM_ALLOC_SIZE
+                  IMAGE_SUPPORT
+                  MAX_READ_IMAGE_ARGS
+                  MAX_WRITE_IMAGE_ARGS
+                  IMAGE2D_MAX_WIDTH
+                  IMAGE2D_MAX_HEIGHT
+                  IMAGE3D_MAX_WIDTH
+                  IMAGE3D_MAX_HEIGHT
+                  IMAGE3D_MAX_DEPTH
+                  MAX_SAMPLERS
+                  MAX_PARAMETER_SIZE
+                  MEM_BASE_ADDR_ALIGN
+                  MIN_DATA_TYPE_ALIGN_SIZE
+                  GLOBAL_MEM_CACHELINE_SIZE
+                  GLOBAL_MEM_CACHE_SIZE
+                  GLOBAL_MEM_SIZE
+                  MAX_CONSTANT_BUFFER_SIZE
+                  MAX_CONSTANT_ARGS
+                  LOCAL_MEM_SIZE
+                  ERROR_CORRECTION_SUPPORT
+                  PROFILING_TIMER_RESOLUTION
+                  ENDIAN_LITTLE
+                  AVAILABLE
+                  COMPILER_AVAILABLE]))
+
+(def str-props (map #(symbol (str "CL_DEVICE_" %))
+               '[NAME
+                 VENDOR
+                 PROFILE
+                 VERSION
+                 EXTENSIONS]))
+
+(def hex-props (map #(symbol (str "CL_DEVICE_" %))
+               '[SINGLE_FP_CONFIG
+                 QUEUE_PROPERTIES]))
+
+(defn parse-unsigned-info [array]
+  (reduce (fn [acc x] (+ (* 256 acc) x))
+          (reverse (map (fn [x] (if (neg? x) (+ x 256) x))
+                        array))))
+(defn parse-str-info [array]
+  (apply str (map char (butlast array))))
+(defn parse-device-type [array]
+  (let [types (map #(symbol (str "CL_DEVICE_TYPE_" %))
+                   '[DEFAULT CPU GPU ACCELERATOR])
+        type-vals (map #(.get (.getField org.jocl.CL (str %)) nil)
+                       types)
+        u (parse-unsigned-info array)]
+    (vec (map first
+              (remove #(= 0 (get % 1))
+                      (map (fn [t tv] [t (bit-and u tv)]) types type-vals)
+                      )))))
+(defn parse-size-t-array [array]
+  (vec (map parse-unsigned-info
+            (partition org.jocl.Sizeof/size_t array)
+            )))
+
+(defn get-device [device]
+  (let [long-info (map #(clGetDeviceInfo device %)
+                       long-props)
+        str-info (map #(clGetDeviceInfo device %)
+                      str-props)
+        hex-info (map #(clGetDeviceInfo device %)
+                      hex-props)]
+    {:id   device
+     :info (concat (map vector long-props (map parse-unsigned-info long-info))
+                   (map vector str-props (map parse-str-info str-info))
+                   (map vector hex-props (map parse-unsigned-info hex-info))
+                   [['CL_DEVICE_TYPE
+                    (parse-device-type
+                     (clGetDeviceInfo device 'CL_DEVICE_TYPE))]
+                    ['CL_DEVICE_MAX_WORK_ITEM_SIZES
+                     (parse-size-t-array
+                      (clGetDeviceInfo device
+                       'CL_DEVICE_MAX_WORK_ITEM_SIZES))]])}))
+
+(defn get-platform [platform]
+  (let [names '[CL_PLATFORM_PROFILE
+                CL_PLATFORM_VERSION
+                CL_PLATFORM_NAME
+                CL_PLATFORM_VENDOR
+                CL_PLATFORM_EXTENSIONS]]
+    {:id      platform
+     :info    (concat 
+               (map vector
+                names
+                (map #(parse-str-info (clGetPlatformInfo platform %))
+                     names)))
+     :devices (map get-device (clGetDeviceIDs platform))
+     }))
