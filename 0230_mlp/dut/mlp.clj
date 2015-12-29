695744d495ddb8bbb8542dd4b140f091c888f96b
unknown <tyam@N34125.office.amsiag.com>
2015-12-29 14:20:23 +0100

    - Refactorred begin aimed at OpenCL. Under debugging.

--- 0230_mlp/dut/mlp.clj
+++ 0230_mlp/dut/mlp.clj
@@ -32,6 +32,76 @@
 ; exps: vector of exp(-z)
 ; sigs: vector of 1/(1+exp(-z)) = s(z)
 
+; niv : neuron input vector
+; nov : neuron output vector
+; ndv : neuron output derivative vector
+; psv : propagated slope vector
+; wm  : weight matrix
+; wam : weight matrix to be accumulated
+; bv  : intercept vector
+; bav : intercept vector to be accumulated
+; eov : expected output vector
+; edv : error derivative vector
+
+(defn fw1 [{wm :wm bv :bv} niv]
+  (let [weighed (map #(+ (reduce + (map * %1 niv)) %2) wm bv)
+        expv (map #(Math/exp (- %)) weighed)
+        nov (map #(/ (+ 1.0 %)) expv)]
+    {:nov nov :ndv (map #(* %1 %2 %2) expv nov)}))
+
+(defn bw1 [{wm :wm bm :bm} niv psv {wam :wam bav :bav}]
+  {:wam (map (fn [wrow ps] (map (fn [wel ni] (+ wel (* ni ps)))
+                                wrow niv))
+             wm psv)
+   :bav (map + bav psv)
+   :psv (reduce #(map + %1 %2)
+                (map (fn [wrow ps] (map #(* % ps)) wrow)
+                     wm psv))})
+
+(defn fw [wbs niv]
+  (loop [wbs wbs nivs [niv] ndvs []]
+    (if (empty? wbs)
+      {:nivs nivs :ndvs ndvs}
+      (let [{nov :nov ndv :ndv} (fw1 (first wbs) (last nivs))]
+        (recur (next wbs) (conj nivs nov) (conj ndvs ndv))
+        ))))
+
+(defn bw [wbs {nivs :nivs ndvs :ndvs} eov wbas speed]
+  (loop [wbs wbs nivs (butlast nivs) ndvs ndvs wbas wbas
+         psv (map #(* (- %1 %2) speed) eov (last nivs)) acc []]
+    (if (empty? wbs)
+      acc
+      (let [{wam :wam bav :bav psv :psv}
+            (bw1 (last wbs) (last nivs) psv (last wbas))]
+        (recur (butlast wbs) (butlast nivs) (butlast ndvs) (butlast wbas)
+               psv (conj acc {:wam wam :bav bav})
+               )))))
+
+(defn init-wbas [wbs]
+  (vec (map (fn [{wm :wm bv :bv}]
+              {:wm (vec (repeat (count wm)
+                                (vec (repeat (count (first wm))
+                                             0.0))))
+               :bv (vec (repeat (count bv) 0.0))})
+            wbs)))
+
+(defn learn1 [wbs training-data speed]
+  (loop [td training-data wbas (init-wbas wbs)]
+    (if (empty? td)
+      (r+ wbas)
+      (recur (next td)
+             (bw wbs (fw wbs (:niv (first td)))
+                 (:eov (first td)) wbas speed
+                 )))))
+
+(defn init-wbs [n-neuron-vector]
+  (vec (map (fn [wcolc bc]
+              {:wm (vec (repeat bc 
+                                (vec (repeat wcolc 0.0))))
+               :bv (vec (repeat bc 0.0))})
+            n-neuron-vector
+            (next n-neuron-vector))))
+
 (defn calc-output [weight-array b-array invec]
   (loop [ws weight-array, bs b-array, out [invec], deriv []]
     (if (or (empty? ws) (empty? bs))
