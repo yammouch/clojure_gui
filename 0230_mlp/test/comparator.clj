706d1abe8e2b228b171ec311c65cee57ef5e1252
unknown <tyam@N34125.office.amsiag.com>
2015-12-30 04:20:36 +0100

    - Now error is shown.

--- 0230_mlp/test/comparator.clj
+++ 0230_mlp/test/comparator.clj
@@ -1,7 +1,6 @@
 (ns comparator)
 
 (require 'mlp)
-(require 'clojure.pprint)
 
 (def training-data
   [{:niv [0.0] :eov [0.0]}
@@ -10,10 +9,10 @@
    {:niv [3.0] :eov [1.0]}])
 
 (loop [i 0 wbs (mlp/init-wbs [1 1])]
-  (if (<= 1000 i)
-    :done
-    (let [wbs-next (mlp/learn1 wbs training-data 0.025)]
-      (when (= (mod i 50) 0)
-        (println "iter: " i)
-        (clojure.pprint/pprint wbs))
-      (recur (inc i) wbs-next))))
+  (when (= (mod i 1000) 0)
+    (let [err (mlp/calc-err wbs training-data)]
+      (printf "iter: %5d  avg err: %.3f  max err: %.3f\n"
+              i (:avg err) (:max err))))
+  (if (<= 10000 i)
+    (prn wbs)
+    (recur (inc i) (mlp/learn1 wbs training-data 0.025))))
