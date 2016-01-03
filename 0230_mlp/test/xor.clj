7496cfb32269212e95b2de6f390cf0cad5406d77
unknown <tyam@N34125.office.amsiag.com>
2016-01-03 13:15:53 +0100

    - Previous test pattern also works.

--- 0230_mlp/test/xor.clj
+++ 0230_mlp/test/xor.clj
@@ -16,23 +16,23 @@
    {:niv [1.0 1.0] :eov [0.0]}])
 
 (loop [i 0
-       wbs [{:wm [[2.0 2.0]
-                  [5.8 5.8]
-                  [2.6 2.6]]
-             :bv [-3.2 -2.4 -4.1]}
-            {:wm [[2.1 -3.8 2.6]
-                  [3.3 -5.4 4.2]
-                  [1.8 -3.8 2.4]]
-             :bv [0.8 1.4 0.9]}
-            {:wm [[-4.5 -7.3 -4.3]]
-             :bv [6.5]}]
-       rnd (map #(mod % (count training-data)) (lcg 1))]
+       wbs [{:wm [[0.0 0.0]
+                  [0.0 0.1]
+                  [0.0 0.0]]
+             :bv [0.1 0.0 -0.1]}
+            {:wm [[0.1  0.0 0.0]
+                  [0.0 -0.1 0.0]
+                  [0.0  0.0 0.0]]
+             :bv [0.0 0.0 0.0]}
+            {:wm [[0.0 0.0 0.0]]
+             :bv [0.0]}]
+       rnd (map #(mod (mod % 131) (count training-data)) (lcg 1))]
   (when (= (mod i 10000) 0)
     (let [err (mlp/calc-err wbs training-data)]
       (printf "iter: %7d  avg err: %.3f  max err: %.3f\n"
               i (:avg err) (:max err)))
     (flush))
-  (if (<= 1000000 i)
+  (if (<= 4000000 i)
     (prn wbs)
     (recur (inc i)
            (mlp/learn1 wbs
