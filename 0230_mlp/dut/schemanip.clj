0e478dae1fc9d2c767129fbe79e8dcb95cefe14b
Takuya Yamauchi <yammouch@gmail.com>
2016-01-17 10:40:48 +0900

    - Updated.

--- 0230_mlp/dut/schemanip.clj
+++ 0230_mlp/dut/schemanip.clj
@@ -6,15 +6,15 @@
   (if (or (= (get-in cmd [:org 1]) 0) nil
           (and (= (:cmd cmd) :move-y)
                (= (:dst cmd) 0))
-          (some (partial some zero?)
+          (some (partial some (complement zero?))
                 (first (:body field))))
     nil
     (-> (if (= (:cmd cmd) :move-y)
           (update-in x [:cmd :dst] dec)
           x)
-        (assoc :field (take (get-in field [:size 1])
-                            (cons (reduce #(repeat %2 %1) 0
-                                          [(count (get-in field [:body 0 0]))
-                                           (get-in field [:size 0])])
-                                  (get field :body))))
+        (assoc-in [:field :body]
+                  (concat (next (get field :body))
+                          [(reduce #(repeat %2 %1) 0
+                                   [(count (get-in field [:body 0 0]))
+                                    (get-in field [:size 0])])]))
         (update-in [:cmd :org 1] dec))))
