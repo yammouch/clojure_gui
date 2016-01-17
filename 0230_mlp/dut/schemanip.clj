6c28890c4a6dd3241c70e0646e0e54510dde652e
Tackya Yammouch <yammouch@gmail.com>
2016-01-17 21:28:12 +0900

    - 'slide-lower' has been added.

--- 0230_mlp/dut/schemanip.clj
+++ 0230_mlp/dut/schemanip.clj
@@ -2,8 +2,15 @@
 
 ; {:cmd :move-x :org [4 6] :dst 8}
 
+(defn slide-upper-field [field]
+  (assoc field :body
+         (concat (vec (next (get field :body)))
+                 [(reduce #(repeat %2 %1) 0
+                          [(count (get-in field [:body 0 0]))
+                           (get-in field [:size 0])])])))
+
 (defn slide-upper [{field :field cmd :cmd :as x}]
-  (if (or (= (get-in cmd [:org 1]) 0) nil
+  (if (or (= (get-in cmd [:org 1]) 0)
           (and (= (:cmd cmd) :move-y)
                (= (:dst cmd) 0))
           (some (partial some (complement zero?))
@@ -12,9 +19,25 @@
     (-> (if (= (:cmd cmd) :move-y)
           (update-in x [:cmd :dst] dec)
           x)
-        (assoc-in [:field :body]
-                  (concat (next (get field :body))
-                          [(reduce #(repeat %2 %1) 0
-                                   [(count (get-in field [:body 0 0]))
-                                    (get-in field [:size 0])])]))
+        (update-in [:field] slide-upper-field)
         (update-in [:cmd :org 1] dec))))
+
+(defn slide-lower-field [field]
+  (assoc field :body
+         (cons (reduce #(repeat %2 %1) 0
+                       [(count (get-in field [:body 0 0]))
+                        (get-in field [:size 0])])
+               (butlast (get field :body)))))
+
+(defn slide-lower [{field :field cmd :cmd :as x}]
+  (if (or (<= (get-in field [:size 1]) (get-in cmd [:org 1]))
+          (and (= (:cmd cmd) :move-y)
+               (<= (get-in field [:size 1]) (:dst cmd)))
+          (some (partial some (complement zero?))
+                (last (:body field))))
+    nil
+    (-> (if (= (:cmd cmd) :move-y)
+          (update-in x [:cmd :dst] inc)
+          x)
+        (update-in [:field] slide-lower-field)
+        (update-in [:cmd :org 1] inc))))
--- 0230_mlp/test/test_schemanip.clj
+++ 0230_mlp/test/test_schemanip.clj
@@ -16,11 +16,44 @@
                    :body [[[0 1] [0 0]]
                           [[0 0] [0 0]]
                           [[0 0] [0 0]]]}
-           :cmd {:cmd :move-y :org [1 1] :dst 0}
-           }}])
+           :cmd {:cmd :move-y :org [1 1] :dst 0}}}
+   {:arg  {:field {:size [2 3]
+                   :body [[[0 0] [0 0]]
+                          [[0 1] [0 0]]
+                          [[0 0] [0 0]]]}
+           :cmd {:cmd :move-y :org [1 2] :dst 0}}
+    :expc nil
+    }])
 
 (doseq [{arg :arg expc :expc} test-patterns]
   (let [result (smp/slide-upper arg)]
+    (if (= result expc)
+      (print "[OK]")
+      (print "[ER]" result))
+    (println " test case " arg)))
+
+(println "Tests slide-lower.")
+
+(def test-patterns
+  [{:arg  {:field {:size [2 3]
+                   :body [[[0 0] [0 0]]
+                          [[0 1] [0 0]]
+                          [[0 0] [0 0]]]}
+           :cmd {:cmd :move-y :org [1 1] :dst 0}}
+    :expc {:field {:size [2 3]
+                   :body [[[0 0] [0 0]]
+                          [[0 0] [0 0]]
+                          [[0 1] [0 0]]]}
+           :cmd {:cmd :move-y :org [1 2] :dst 1}}}
+   {:arg  {:field {:size [2 3]
+                   :body [[[0 0] [0 0]]
+                          [[0 0] [0 0]]
+                          [[0 0] [1 0]]]}
+           :cmd {:cmd :move-y :org [1 1] :dst 0}}
+    :expc nil}])
+
+(doseq [{arg :arg expc :expc} test-patterns]
+  (let [result (smp/slide-lower arg)]
     (if (= result expc)
       (print "[OK]")
       (print "[ER]" result))
