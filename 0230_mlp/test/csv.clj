bfaf3db61dbc4286f966309c7ce2d5bea5cc7789
Takuya Yamauchi <tyam@office.amsiag.com>
2016-04-27 18:34:54 +0900

    Updated.

--- 0230_mlp/test/csv.clj
+++ 0230_mlp/test/csv.clj
@@ -1,5 +1,9 @@
+(defn l2i [l]
+  (format "%02X" (reduce #(+ (* 2 %1) %2) 0 l)))
+
 (let [x (read-string (slurp "td000.txt"))]
   (print (apply str (interpose "\n"
-                              (map (fn [p] (apply str (interpose "," p)))
-                                   (apply concat (:body x))
+                              (map (fn [row]
+			             (apply str (interpose "," (map l2i row))))
+                                   (:body x)
 			           )))))
